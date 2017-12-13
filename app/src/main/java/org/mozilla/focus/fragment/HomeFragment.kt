b70c18085f7f408603570e0514bd7467c635af45
Michael Comella <michael.l.comella@gmail.com>
2017-12-13 16:14:10 -0600

    Add tile RecyclerView to home fragment with fillers.

--- app/src/main/java/org/mozilla/focus/fragment/HomeFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/HomeFragment.kt
@@ -6,15 +6,35 @@ package org.mozilla.focus.fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import org.mozilla.focus.R
+
+import kotlinx.android.synthetic.main.fragment_home.*
+
+private const val COL_COUNT = 5
 
 /** The home fragment which displays the navigation tiles of the app. */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflater!!.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // todo: saved instance state?
+        initTiles()
+    }
+
+    // todo: resize tiles.
+    private fun initTiles() = with (tileContainer) {
+        adapter = HomeTileAdapter()
+        layoutManager = GridLayoutManager(context, COL_COUNT)
+        setHasFixedSize(true)
+    }
 
     companion object {
         const val FRAGMENT_TAG = "home"
@@ -23,3 +43,21 @@ class HomeFragment : Fragment() {
         fun create() = HomeFragment()
     }
 }
+
+private const val TILE_COUNT = 10
+
+private class HomeTileAdapter : RecyclerView.Adapter<TileViewHolder>() {
+
+    override fun onBindViewHolder(holder: TileViewHolder, position: Int) {
+    }
+
+    override fun getItemCount() = TILE_COUNT
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = TileViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.home_tile, parent, false)
+    )
+}
+
+private class TileViewHolder(
+        itemView: View
+) : RecyclerView.ViewHolder(itemView)
