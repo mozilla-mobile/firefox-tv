71fdad4cfdf168782807c1ab0c6cc3f430aa7d4f
Bryan Munar <brymunar@gmail.com>
2017-12-15 10:24:01 -0600

    Added Telemetry checkbox functionality

--- app/src/main/java/org/mozilla/focus/fragment/NewSettingsFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/NewSettingsFragment.kt
@@ -7,18 +7,80 @@ package org.mozilla.focus.fragment
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
+import org.mozilla.focus.telemetry.TelemetryWrapper
+import android.app.AlertDialog
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.fragment_new_settings.*
 import org.mozilla.focus.R
+import android.R.string.cancel
+import android.content.DialogInterface
+import org.mozilla.focus.session.SessionManager
+
 
 /** The home fragment which displays the navigation tiles of the app. */
 class NewSettingsFragment : Fragment() {
+    lateinit var telemetryButton: ImageView;
+    lateinit var deleteButton: ImageView;
+    lateinit var aboutButton: ImageView;
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?) =
-            inflater!!.inflate(R.layout.fragment_new_settings, container, false)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View?  {
+        val rootView = inflater!!.inflate(R.layout.fragment_new_settings, container, false)
+        telemetryButton = rootView.findViewById<ImageView>(R.id.telemetryButton)
+        telemetryButton.setOnClickListener { view ->
+            val telemetryEnabled = !TelemetryWrapper.isTelemetryEnabled(activity)
+            TelemetryWrapper.setTelemetryEnabled(activity, telemetryEnabled)
+            if (telemetryEnabled) {
+                telemetryButton.setImageResource(R.drawable.ic_checkmark_enabled)
+            } else {
+                telemetryButton.setImageResource(R.drawable.ic_checkmark_disabled)
+            }
+        }
+
+        deleteButton = rootView.findViewById(R.id.deleteButton)
+        deleteButton.setOnClickListener { view ->
+            val builder1 = AlertDialog.Builder(activity)
+            builder1.setTitle("Clear all cookies and site data")
+            builder1.setMessage("Selecting ‘Clear Now’ will clear all cookies and site data stored by Firefox for Fire TV. This may sign you out of websites and remove offline web content.")
+            builder1.setCancelable(true)
+
+            builder1.setPositiveButton(
+                    "OK",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        SessionManager.getInstance().removeAllSessions()
+                        dialog.cancel() })
+
+            builder1.setNegativeButton(
+                    "Cancel",
+                    DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+
+            val alert11 = builder1.create()
+            alert11.show()
+        }
+
+        aboutButton = rootView.findViewById(R.id.aboutButton)
+        aboutButton.setOnClickListener { view ->
+            val builder1 = AlertDialog.Builder(activity)
+            builder1.setTitle("Clear all cookies and site data")
+            builder1.setMessage("Selecting ‘Clear Now’ will clear all cookies and site data stored by Firefox for Fire TV. This may sign you out of websites and remove offline web content.")
+            builder1.setCancelable(true)
+
+            builder1.setPositiveButton(
+                    "OK",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        SessionManager.getInstance().removeAllSessions()
+                        dialog.cancel() })
+
+            builder1.setNegativeButton(
+                    "Cancel",
+                    DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+
+            val alert11 = builder1.create()
+            alert11.show()
+        }
+        return rootView
+    }
 
     companion object {
         const val FRAGMENT_TAG = "new_settings"
