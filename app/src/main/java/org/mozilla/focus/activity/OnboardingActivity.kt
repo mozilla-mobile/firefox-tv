a396b6a7bebef518499c0152d807a9149be36431
Jeff Boek <jeff@jeffboek.com>
2018-01-19 18:12:45 -0800

    call finish when done with activity

--- app/src/main/java/org/mozilla/focus/activity/OnboardingActivity.kt
+++ app/src/main/java/org/mozilla/focus/activity/OnboardingActivity.kt
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import org.mozilla.focus.R
 
 import kotlinx.android.synthetic.main.activity_onboarding.*
+import kotlinx.android.synthetic.main.content_onboarding.*
 
 class OnboardingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
-    }
 
+        enable_turbo_mode.setOnClickListener { view ->
+            
+            finish()
+        }
+
+        disable_turbo_mode.setOnClickListener { view ->
+            finish()
+        }
+    }
 }
