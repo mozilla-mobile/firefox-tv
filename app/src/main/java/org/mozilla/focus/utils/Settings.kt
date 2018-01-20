de0694951bdaec23594d40f216c6c17fa508105e
Jeff Boek <jeff@jeffboek.com>
2018-01-19 18:46:58 -0800

    Fix boolean logic to check to see onboarding

--- app/src/main/java/org/mozilla/focus/utils/Settings.kt
+++ app/src/main/java/org/mozilla/focus/utils/Settings.kt
@@ -44,7 +44,7 @@ class Settings private constructor(context: Context) {
             false
 
     fun shouldShowFirstRun(): Boolean =
-            preferences.getBoolean(OnboardingActivity.ONBOARD_PREF, false)
+            !preferences.getBoolean(OnboardingActivity.ONBOARD_PREF, false)
 
     fun shouldUseSecureMode(): Boolean =
             preferences.getBoolean(getPreferenceKey(R.string.pref_key_secure), false)
