c3acba40c728e8915b5640ce74609b5b240c5bea
Michael Comella <michael.l.comella@gmail.com>
2018-01-19 19:31:27 -0800

    Re-add broken import.
    I don't know why this rm'd in a previous commit and why I could have pushed
    before given I have pre-push hooks - merge conflict?

--- app/src/main/java/org/mozilla/focus/activity/MainActivity.java
+++ app/src/main/java/org/mozilla/focus/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
