72e0ea7da87db075593cd37657e49a6e19df4eb9
Michael Comella <michael.l.comella@gmail.com>
2018-01-24 18:43:27 -0800

    Issue #405: Reorder and group related initWebSettings calls.
    Also rm redundant comments - this should improve readability.

--- app/src/amazonWebview/java/org/mozilla/focus/web/WebViewProvider.kt
+++ app/src/amazonWebview/java/org/mozilla/focus/web/WebViewProvider.kt
@@ -49,48 +49,39 @@ private fun initWebview(webView: AmazonWebView) = with (webView) {
 }
 
 @SuppressLint("SetJavaScriptEnabled") // We explicitly want to enable JavaScript
-private fun initWebSettings(context: Context, settings: AmazonWebSettings) {
-    settings.javaScriptEnabled = true
+private fun initWebSettings(context: Context, settings: AmazonWebSettings) = with (settings) {
+    val appName = context.resources.getString(R.string.useragent_appname)
+    userAgentString = UserAgent.buildUserAgentString(context, settings, appName)
 
-    // Needs to be enabled to display some HTML5 sites that use local storage
-    settings.domStorageEnabled = true
+    javaScriptEnabled = true
+    domStorageEnabled = true
 
-    // Enabling built in zooming shows the controls by default
-    settings.builtInZoomControls = true
+    // The default for those settings should be "false" - But we want to be explicit.
+    setAppCacheEnabled(false)
+    databaseEnabled = false
+    javaScriptCanOpenWindowsAutomatically = false
 
-    // So we hide the controls after enabling zooming
-    settings.displayZoomControls = false
+    saveFormData = false
+    savePassword = false
 
-    // To respect the html viewport:
-    settings.loadWithOverviewMode = true
+    setGeolocationEnabled(false) // We do not implement the callbacks
+
+    builtInZoomControls = true
+    displayZoomControls = false // Hide by default
+
+    loadWithOverviewMode = true // To respect the html viewport
 
     // Also increase text size to fill the viewport (this mirrors the behaviour of Firefox,
     // Chrome does this in the current Chrome Dev, but not Chrome release).
-    // TODO TEXT_AUTOSIZING does not exist in AmazonWebSettings
+    // TODO #33: TEXT_AUTOSIZING does not exist in AmazonWebSettings
     //settings.setLayoutAlgorithm(AmazonWebSettings.LayoutAlgorithm.TEXT_AUTOSIZING);
 
     // Disable access to arbitrary local files by webpages - assets can still be loaded
     // via file:///android_asset/res, so at least error page images won't be blocked.
-    settings.allowFileAccess = false
-    settings.allowFileAccessFromFileURLs = false
-    settings.allowUniversalAccessFromFileURLs = false
-
-    val appName = context.resources.getString(R.string.useragent_appname)
-    settings.userAgentString = UserAgent.buildUserAgentString(context, settings, appName)
+    allowFileAccess = false
+    allowFileAccessFromFileURLs = false
+    allowUniversalAccessFromFileURLs = false
 
     // Right now I do not know why we should allow loading content from a content provider
-    settings.allowContentAccess = false
-
-    // The default for those settings should be "false" - But we want to be explicit.
-    settings.setAppCacheEnabled(false)
-    settings.databaseEnabled = false
-    settings.javaScriptCanOpenWindowsAutomatically = false
-
-    // We do not implement the callbacks - So let's disable it.
-    settings.setGeolocationEnabled(false)
-
-    // We do not want to save any data...
-    settings.saveFormData = false
-
-    settings.savePassword = false
+    allowContentAccess = false
 }
