671e23c943579a37ae10638160ec1b0d8977c3e9
Michael Comella <michael.l.comella@gmail.com>
2018-02-01 17:53:36 -0800

    Issue #412: Add WebView scrolling to Cursor.

--- app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/focus/fragment/BrowserFragment.kt
@@ -28,11 +28,9 @@ import org.mozilla.focus.session.SessionManager
 import org.mozilla.focus.telemetry.TelemetryWrapper
 import org.mozilla.focus.telemetry.UrlTextInputLocation
 import org.mozilla.focus.utils.Direction
-import org.mozilla.focus.utils.Edge
 import org.mozilla.focus.web.IWebView
 import org.mozilla.focus.widget.BrowserNavigationOverlay
 import org.mozilla.focus.widget.Cursor
-import org.mozilla.focus.widget.CursorEvent
 import org.mozilla.focus.widget.InlineAutocompleteEditText
 import org.mozilla.focus.widget.NavigationEvent
 
@@ -42,7 +40,7 @@ private const val SCROLL_MULTIPLIER = 45
 /**
  * Fragment for displaying the browser UI.
  */
-class BrowserFragment : WebFragment(), CursorEvent, BrowserNavigationOverlay.NavigationEventHandler {
+class BrowserFragment : WebFragment(), BrowserNavigationOverlay.NavigationEventHandler {
     companion object {
         const val FRAGMENT_TAG = "browser"
 
@@ -65,8 +63,6 @@ class BrowserFragment : WebFragment(), CursorEvent, BrowserNavigationOverlay.Nav
     private lateinit var session: Session
 
     private val cursorViewModel = CursorViewModel(simulateTouchEvent = { activity.dispatchTouchEvent(it) })
-
-    private val scrollVelocity get() = cursor.speed.toInt() * SCROLL_MULTIPLIER
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -126,13 +122,15 @@ class BrowserFragment : WebFragment(), CursorEvent, BrowserNavigationOverlay.Nav
 
     override fun inflateLayout(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
             inflater.inflate(R.layout.fragment_browser, container, false).apply {
-                cursor.cursorEvent = this@BrowserFragment
                 connectCursorToViewModel(cursor)
             }
 
     @UiThread // CursorViewModel.onUpdate requires.
     private fun connectCursorToViewModel(cursor: Cursor) {
-        cursorViewModel.onUpdate = { x, y -> cursor.updatePosition(x, y) }
+        cursorViewModel.onUpdate = { x, y, scrollVel ->
+            cursor.updatePosition(x, y)
+            scrollWebView(scrollVel)
+        }
         cursor.onLayout = { width, height ->
             cursorViewModel.maxBounds = PointF(width.toFloat(), height.toFloat())
         }
@@ -201,15 +199,10 @@ class BrowserFragment : WebFragment(), CursorEvent, BrowserNavigationOverlay.Nav
         cursor.visibility = if (toEnable) View.VISIBLE else View.GONE
     }
 
-    override fun cursorHitEdge(edge: Edge) {
-        val webView = webView ?: return
-
-        when (edge) {
-            Edge.TOP -> webView.flingScroll(0, -scrollVelocity)
-            Edge.BOTTOM -> webView.flingScroll(0, scrollVelocity)
-            Edge.LEFT -> webView.flingScroll(-scrollVelocity, 0)
-            Edge.RIGHT -> webView.flingScroll(scrollVelocity, 0)
-        }
+    private fun scrollWebView(scrollVel: PointF) {
+        val scrollX = Math.round(scrollVel.x * SCROLL_MULTIPLIER)
+        val scrollY = Math.round(scrollVel.y * SCROLL_MULTIPLIER)
+        webview?.flingScroll(scrollX, scrollY)
     }
 }
 
