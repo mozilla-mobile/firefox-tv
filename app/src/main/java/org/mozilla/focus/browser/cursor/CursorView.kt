a6bbe9a8bd01ac469515c25d3c709d632ef35254
Michael Comella <michael.l.comella@gmail.com>
2018-02-07 14:14:18 -0800

    Issue #412: Don't allocate in onDraw.
    The update loop still allocates (due to iterators and the coroutine framework)
    but this should make it easier to reduce allocations later.

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -7,49 +7,44 @@ package org.mozilla.focus.browser.cursor
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.PointF
 import android.graphics.RadialGradient
 import android.graphics.Shader
 import android.support.annotation.UiThread
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import org.mozilla.focus.R
 
-private const val CURSOR_SIZE = 45f
 private const val CURSOR_ALPHA = 102
 
 /** A drawn Cursor: see [CursorViewModel] for responding to keys and setting position. */
 class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
-    /** A callback when layout for this view occurs. */
-    var onLayoutChanged: (width: Int, height: Int) -> Unit = { _, _ -> }
-
-    private val pos = PointF()
-
+    // The radius is half the size of the container its drawn in.
+    private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
     private val paint = Paint().apply {
         style = Paint.Style.FILL
         alpha = CURSOR_ALPHA
         isAntiAlias = true
+        shader = RadialGradient(radius, radius, /* radius */ radius,
+                ContextCompat.getColor(context, R.color.teal50), ContextCompat.getColor(context, R.color.photonBlue50),
+                Shader.TileMode.CLAMP)
     }
 
     @UiThread
     fun updatePosition(x: Float, y: Float) {
-        pos.set(x, y)
-        invalidate()
+        // In onDraw, we offset the initial position from the origin: we undo that offset here.
+        translationX = x - radius
+        translationY = y - radius
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        paint.shader = RadialGradient(pos.x, pos.y, 45f, resources.getColor(R.color.teal50), resources.getColor(R.color.photonBlue50), Shader.TileMode.CLAMP)
-        canvas.drawCircle(pos.x, pos.y, CURSOR_SIZE, paint)
-    }
-
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-
-        if (changed) {
-            onLayoutChanged(right, bottom)
-        }
+        // We draw the cursor in the top-left corner, offset so the circle isn't clipped by its
+        // parent. The translationX/Y properties will draw it into the desired position on the
+        // screen. We do it this way to avoid allocating new RadialGradients, which are
+        // constructed in relation to the current position of the circle.
+        canvas.drawCircle(radius, radius, radius, paint)
     }
 }
