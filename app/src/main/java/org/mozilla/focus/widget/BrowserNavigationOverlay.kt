87a3260cf1d1415ccbc916386406a1f50e25ac4b
Chenxia Liu <liuche@mozilla.com>
2018-02-21 15:39:22 -0800

    Fix unused imports lint.

--- app/src/main/java/org/mozilla/focus/widget/BrowserNavigationOverlay.kt
+++ app/src/main/java/org/mozilla/focus/widget/BrowserNavigationOverlay.kt
@@ -5,8 +5,6 @@
 package org.mozilla.focus.widget
 
 import android.content.Context
-import android.graphics.Color
-import android.support.v4.graphics.drawable.DrawableCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
