d5b0a8e77690e4ae5c0088b515e3e801b508777d
Jeff Boek <jeff@jeffboek.com>
2018-02-23 14:31:39 -0800

    Fixes #489 - Updates the visual style of the cursor

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -6,14 +6,10 @@ package org.mozilla.focus.browser.cursor
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.RadialGradient
-import android.graphics.Shader
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.support.annotation.UiThread
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import org.mozilla.focus.R
@@ -33,23 +29,13 @@ private val HIDE_AFTER_MILLIS = TimeUnit.SECONDS.toMillis(3)
 class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val hideHandler = CursorHideHandler(this)
-
-    // The radius is half the size of the container its drawn in.
-    private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
-    private val paint = Paint().apply {
-        style = Paint.Style.FILL
-        alpha = CURSOR_ALPHA
-        isAntiAlias = true
-        shader = RadialGradient(radius, radius, /* radius */ radius,
-                ContextCompat.getColor(context, R.color.teal50), ContextCompat.getColor(context, R.color.photonBlue50),
-                Shader.TileMode.CLAMP)
-    }
+    private val CROSSHAIR_OFFSET = 6
 
     @UiThread
     fun updatePosition(x: Float, y: Float) {
-        // In onDraw, we offset the initial position from the origin: we undo that offset here.
-        translationX = x - radius
-        translationY = y - radius
+        // Offset the position to better align with the crosshairs.
+        translationX = x - CROSSHAIR_OFFSET
+        translationY = y - CROSSHAIR_OFFSET
 
         setMaxVisibility()
         resetCountdown()
@@ -58,11 +44,9 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // We draw the cursor in the top-left corner, offset so the circle isn't clipped by its
-        // parent. The translationX/Y properties will draw it into the desired position on the
-        // screen. We do it this way to avoid allocating new RadialGradients, which are
-        // constructed in relation to the current position of the circle.
-        canvas.drawCircle(radius, radius, radius, paint)
+        val cursor = context.resources.getDrawable(R.drawable.pointer, context.theme)
+        cursor.setBounds(0, 0, height, width)
+        cursor.draw(canvas)
     }
 
     fun cancelUpdates() {
