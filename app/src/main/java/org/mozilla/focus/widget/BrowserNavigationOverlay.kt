62455c3df9c09c41fc10d9e67252c00023844219
Jeff Boek <jeff@jeffboek.com>
2018-02-27 14:36:13 -0800

    Fixes nits in #529

--- app/src/main/java/org/mozilla/focus/widget/BrowserNavigationOverlay.kt
+++ app/src/main/java/org/mozilla/focus/widget/BrowserNavigationOverlay.kt
@@ -5,6 +5,7 @@
 package org.mozilla.focus.widget
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -76,9 +77,15 @@ class BrowserNavigationOverlay @JvmOverloads constructor(
                 .forEach {
                     it.setOnClickListener(this)
                 }
+
         setupUrlInput()
         turboButton.isChecked = Settings.getInstance(context).isBlockingEnabled
         navButtonSettings.setImageResource(R.drawable.ic_settings)
+
+        navCloseHint.compoundDrawablesRelative.forEach {
+            if (it == null) { return@forEach }
+            it.setTint(ContextCompat.getColor(context, R.color.button_default))
+        }
     }
 
     private fun setupUrlInput() = with (navUrlInput) {
