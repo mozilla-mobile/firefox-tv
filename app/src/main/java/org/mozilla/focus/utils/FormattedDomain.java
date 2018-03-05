2d9b7677048e5973824893c3d20efffa6a95afdc
Michael Comella <michael.l.comella@gmail.com>
2018-03-05 13:40:51 -0800

    Issue #511: URIUtils -> FormattedDomain.format.
    We should be careful about adding too many URI/URL Utils methods so
    let's put this somewhere else for now. Ideally, this would be a
    method on a shared URI class throughout the code base, rather than
    one that operates on Strings.

--- app/src/main/java/org/mozilla/focus/utils/FormattedDomain.java
+++ app/src/main/java/org/mozilla/focus/utils/FormattedDomain.java
@@ -5,7 +5,6 @@
 package org.mozilla.focus.utils;
 
 import android.content.Context;
-import android.os.AsyncTask;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
@@ -15,13 +14,12 @@ import android.util.Patterns;
 
 import org.mozilla.focus.utils.publicsuffix.PublicSuffix;
 
-import java.lang.ref.WeakReference;
 import java.net.URI;
 
-/** Utilities for operating on URLs. */
-public class URIUtils {
+/** Methods for formatting a domain URI. */
+public class FormattedDomain {
 
-    private URIUtils() {}
+    private FormattedDomain() {}
 
     /**
      * Returns the domain for the given URI, formatted by the other available parameters.
@@ -44,8 +42,7 @@ public class URIUtils {
      * - baseDomain [2]: include publicSuffix, 0 subdomains
      *
      * Expressing the method this way (instead of separate baseDomain & hostSLD methods) is more flexible if we want to
-     * change the subdomain count and works well with our {@link GetFormattedDomainAsyncTask}, which can take the
-     * same parameters we pass in here, rather than creating a new Task for each method.
+     * change the subdomain count.
      *
      * [1]: https://github.com/mozilla-mobile/firefox-ios/blob/deb9736c905cdf06822ecc4a20152df7b342925d/Shared/Extensions/NSURLExtensions.swift#L152
      * [2]: https://github.com/mozilla-mobile/firefox-ios/blob/deb9736c905cdf06822ecc4a20152df7b342925d/Shared/Extensions/NSURLExtensions.swift#L205
@@ -59,8 +56,8 @@ public class URIUtils {
      */
     @NonNull
     @WorkerThread // calls PublicSuffix methods.
-    public static String getFormattedDomain(@NonNull final Context context, @NonNull final URI uri,
-            final boolean shouldIncludePublicSuffix, @IntRange(from = 0) final int subdomainCount) {
+    public static String format(@NonNull final Context context, @NonNull final URI uri,
+                                final boolean shouldIncludePublicSuffix, @IntRange(from = 0) final int subdomainCount) {
         if (context == null) { throw new NullPointerException("Expected non-null Context argument"); }
         if (uri == null) { throw new NullPointerException("Expected non-null uri argument"); }
         if (subdomainCount < 0) { throw new IllegalArgumentException("Expected subdomainCount >= 0."); }
