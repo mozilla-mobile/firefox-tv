e7c2d552ed01a99c79e7bbd736a6b28586ec2594
Jeff Boek <jeff@jeffboek.com>
2018-03-13 16:34:30 -0700

    Fixes #489 - Updates the visual style of the cursor

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -5,22 +5,18 @@
 package org.mozilla.focus.browser.cursor
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.RadialGradient
-import android.graphics.Shader
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.support.annotation.UiThread
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import org.mozilla.focus.R
 import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
-
-private const val CURSOR_ALPHA = 102
 
 private const val HIDE_MESSAGE_ID = 0
 private const val HIDE_ANIMATION_DURATION_MILLIS = 250L
@@ -38,12 +34,11 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
     private val paint = Paint().apply {
         style = Paint.Style.FILL
-        alpha = CURSOR_ALPHA
         isAntiAlias = true
-        shader = RadialGradient(radius, radius, /* radius */ radius,
-                ContextCompat.getColor(context, R.color.teal50), ContextCompat.getColor(context, R.color.photonBlue50),
-                Shader.TileMode.CLAMP)
     }
+
+    private val cursorBg = BitmapFactory.decodeResource(resources, R.drawable.cursor_bg)
+    private val cursorFg = BitmapFactory.decodeResource(resources, R.drawable.cursor_fg)
 
     @UiThread
     fun updatePosition(x: Float, y: Float) {
@@ -58,11 +53,8 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // We draw the cursor in the top-left corner, offset so the circle isn't clipped by its
-        // parent. The translationX/Y properties will draw it into the desired position on the
-        // screen. We do it this way to avoid allocating new RadialGradients, which are
-        // constructed in relation to the current position of the circle.
-        canvas.drawCircle(radius, radius, radius, paint)
+        canvas.drawBitmap(cursorBg, 0f, 0f, paint)
+        canvas.drawBitmap(cursorFg, 0f, 0f, paint)
     }
 
     fun cancelUpdates() {
