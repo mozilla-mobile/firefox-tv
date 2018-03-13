dbf381b0cc178133105fb2c2eddb9506b9f6661c
Jeff Boek <jeff@jeffboek.com>
2018-03-13 16:34:30 -0700

    Fixes #489 - Updates the visual style of the cursor

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -5,22 +5,20 @@
 package org.mozilla.focus.browser.cursor
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.RadialGradient
-import android.graphics.Shader
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.support.annotation.UiThread
-import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.View
 import org.mozilla.focus.R
 import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
-
-private const val CURSOR_ALPHA = 102
 
 private const val HIDE_MESSAGE_ID = 0
 private const val HIDE_ANIMATION_DURATION_MILLIS = 250L
@@ -38,11 +36,18 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
     private val paint = Paint().apply {
         style = Paint.Style.FILL
-        alpha = CURSOR_ALPHA
         isAntiAlias = true
-        shader = RadialGradient(radius, radius, /* radius */ radius,
-                ContextCompat.getColor(context, R.color.teal50), ContextCompat.getColor(context, R.color.photonBlue50),
-                Shader.TileMode.CLAMP)
+    }
+
+    private val cursorBg = BitmapFactory.decodeResource(resources, R.drawable.cursor_bg)
+    private val cursorBgBlendingMode = PorterDuffXfermode(PorterDuff.Mode.MULTIPLY)
+    private val cursorFg = BitmapFactory.decodeResource(resources, R.drawable.cursor_fg)
+    private val cursorFgBlendingMode = PorterDuffXfermode(PorterDuff.Mode.SCREEN)
+
+    init {
+        // In order to draw the bitmaps with the blending mode and
+        // alpha we have to set the LayerType to software
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null)
     }
 
     @UiThread
@@ -58,11 +63,13 @@ class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // We draw the cursor in the top-left corner, offset so the circle isn't clipped by its
-        // parent. The translationX/Y properties will draw it into the desired position on the
-        // screen. We do it this way to avoid allocating new RadialGradients, which are
-        // constructed in relation to the current position of the circle.
-        canvas.drawCircle(radius, radius, radius, paint)
+        // We have to paint a background for the shadow to apepar
+        canvas.drawRect(0f, 0f, width + 0f, height + 0f, paint)
+
+        paint.xfermode = cursorBgBlendingMode
+        canvas.drawBitmap(cursorBg, 0f, 0f, paint)
+        paint.xfermode = cursorFgBlendingMode
+        canvas.drawBitmap(cursorFg, 0f, 0f, paint)
     }
 
     fun cancelUpdates() {
