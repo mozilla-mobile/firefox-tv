dc00b65b2858bb5b28a78bcbb4e6adbae65e2bcb
Michael Comella <michael.l.comella@gmail.com>
2018-03-13 16:07:07 -0700

    Issue #425: Remove hard-coded robolectric config path.
    It appears this is no longer necessary with
    unitTests.includeTestResources = true.

--- app/src/test/java/org/mozilla/focus/utils/TestFormattedDomain.java
+++ app/src/test/java/org/mozilla/focus/utils/TestFormattedDomain.java
@@ -10,7 +10,7 @@ import android.text.TextUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mozilla.focus.FirefoxTVTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
 import java.net.URI;
@@ -20,7 +20,7 @@ import java.util.regex.Pattern;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
-@RunWith(FirefoxTVTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 public class TestFormattedDomain {
 
     private final String BUGZILLA_URL = "https://bugzilla.mozilla.org/enter_bug.cgi?format=guided#h=dupes%7CData%20%26%20BI%20Services%20Team%7C";
