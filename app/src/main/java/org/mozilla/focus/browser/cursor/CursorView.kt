7b511ac17a825015a3c3a01ea0db5eeb939245da
Michael Comella <michael.l.comella@gmail.com>
2018-03-21 13:56:03 -0700

    Closes #489: Draw cursor in XML.
    Now that we're not dynamically generating the assets, we can simplify
    with XML.

--- app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
+++ app/src/main/java/org/mozilla/focus/browser/cursor/CursorView.kt
@@ -5,16 +5,12 @@
 package org.mozilla.focus.browser.cursor
 
 import android.content.Context
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Paint
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.support.annotation.UiThread
 import android.util.AttributeSet
-import android.view.View
-import org.mozilla.focus.R
+import android.widget.ImageView
 import java.lang.ref.WeakReference
 import java.util.concurrent.TimeUnit
 
@@ -26,35 +22,18 @@ private val HIDE_AFTER_MILLIS = TimeUnit.SECONDS.toMillis(3)
  * A drawn Cursor: see [CursorViewModel] for responding to keys and setting position.
  * The cursor will hide itself when it hasn't received a location update recently.
  */
-class CursorView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class CursorView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
 
     private val hideHandler = CursorHideHandler(this)
 
-    // The radius is half the size of the container its drawn in.
-    private val radius = context.resources.getDimensionPixelSize(R.dimen.remote_cursor_size) / 2f
-    private val paint = Paint().apply {
-        style = Paint.Style.FILL
-        isAntiAlias = true
-    }
-
-    private val cursorBg = BitmapFactory.decodeResource(resources, R.drawable.cursor_bg)
-    private val cursorFg = BitmapFactory.decodeResource(resources, R.drawable.cursor_fg)
-
     @UiThread
     fun updatePosition(x: Float, y: Float) {
-        // In onDraw, we offset the initial position from the origin: we undo that offset here.
-        translationX = x - radius
-        translationY = y - radius
+        // translation* sets the top-left corner so we offset it in order to center the asset.
+        translationX = x - width / 2
+        translationY = y - height / 2
 
         setMaxVisibility()
         resetCountdown()
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-
-        canvas.drawBitmap(cursorBg, 0f, 0f, paint)
-        canvas.drawBitmap(cursorFg, 0f, 0f, paint)
     }
 
     fun cancelUpdates() {
