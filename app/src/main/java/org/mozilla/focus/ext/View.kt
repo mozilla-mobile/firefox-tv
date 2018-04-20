238f2306c22f5b6e54390d7f553dd4772a8cee1c
Michael Comella <michael.l.comella@gmail.com>
2018-04-19 18:33:33 -0700

    Issue #761: Add View.updateLayoutParams.

--- app/src/main/java/org/mozilla/focus/ext/View.kt
+++ app/src/main/java/org/mozilla/focus/ext/View.kt
@@ -5,5 +5,13 @@
 package org.mozilla.focus.ext
 
 import android.view.View
+import android.view.ViewGroup
 
 val View.isVisible: Boolean get() = (visibility == View.VISIBLE)
+
+/** Updates the layout params with the mutations provided to [mutateLayoutParams]. */
+inline fun View.updateLayoutParams(mutateLayoutParams: (ViewGroup.LayoutParams) -> Unit) {
+    val layoutParams = this.layoutParams
+    mutateLayoutParams(layoutParams)
+    this.layoutParams = layoutParams // Calling setLayoutParams forces them to refresh internally.
+}
