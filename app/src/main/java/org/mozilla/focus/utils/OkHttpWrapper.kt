4d9a4c857dc28f2866ea56319744ec90f6c73123
Michael Comella <michael.l.comella@gmail.com>
2018-04-20 15:05:12 -0700

    Issue #765: Add PocketEndpoint.getGlobalVideoRecommendations.

---
+++ app/src/main/java/org/mozilla/focus/utils/OkHttpWrapper.kt
@@ -0,0 +1,18 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.focus.utils
+
+import okhttp3.OkHttpClient
+import java.io.IOException
+
+/** A holder for the shared OkHttpClient. */
+object OkHttpWrapper {
+    val client = OkHttpClient.Builder().build()
+
+    @JvmStatic
+    fun onLowMemory() {
+        try { client.cache().evictAll() } catch (_: IOException) { /* We don't care. */ }
+    }
+}
