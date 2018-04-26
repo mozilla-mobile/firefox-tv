df400be1081889abb1e7f53844bbeed8333cc2a0
Michael Comella <michael.l.comella@gmail.com>
2018-04-25 17:48:09 -0700

    Closes #765: Connect basic Pocket data to placeholder feed.
    The data will be completely filled in #769.

--- app/src/main/java/org/mozilla/focus/home/pocket/PocketVideoFragment.kt
+++ app/src/main/java/org/mozilla/focus/home/pocket/PocketVideoFragment.kt
@@ -16,15 +16,52 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.fragment_pocket_video.view.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 import org.mozilla.focus.R
 import org.mozilla.focus.ext.updateLayoutParams
 
 /** A feed of Pocket videos. */
 class PocketVideoFragment : Fragment() {
 
+    private lateinit var deferredVideos: PocketVideosDeferred
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // The video feed is typically provided by the creator of this fragment such that the videos
+        // on that view (the home screen) match the ones in this feed. However, if this fragment is
+        // being reconstructed, the data will no longer be in memory so we get retrieve the data again
+        // here. Typically, the data would be provided with `Fragment.getArguments`, which would be
+        // provided to reconstructed fragment but we intentionally don't do that: if the feed is
+        // being reconstructed, the user probably cares more about an up-to-date feed rather than
+        // consistency with the home screen they saw a while ago.
+        if (!this::deferredVideos.isInitialized) {
+            deferredVideos = Pocket.getRecommendedVideos()
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        fun displayFeed(layout: View, videos: PocketVideosDeferred) {
+            layout.videoFeed.gridView.adapter = PocketVideoAdapter(context, videos)
+        }
+
         val layout = inflater.inflate(R.layout.fragment_pocket_video, container, false)
-        layout.videoFeed.gridView.adapter = PocketVideoAdapter(context)
+
+        if (deferredVideos.isCompleted) {
+            displayFeed(layout, deferredVideos)
+        } else {
+            // TODO: #769?: display loading screen.
+
+            launch(UI) {
+                val videos = deferredVideos.await()
+                if (videos != null) {
+                    displayFeed(layout, deferredVideos)
+                } else {
+                    // TODO: #769: display error screen.
+                }
+            }
+        }
 
         // SVGs can have artifacts if we set them in XML so we set it in code.
         layout.pocketWordmarkView.setImageDrawable(context.getDrawable(R.drawable.ic_pocket_and_wordmark).apply {
@@ -34,11 +71,17 @@ class PocketVideoFragment : Fragment() {
     }
 
     companion object {
-        fun create() = PocketVideoFragment()
+        fun create(videos: PocketVideosDeferred) = PocketVideoFragment().apply {
+            // See onCreate for why we don't use `arguments`.
+            this.deferredVideos = videos
+        }
     }
 }
 
-private class PocketVideoAdapter(context: Context) : RecyclerView.Adapter<PocketVideoViewHolder>() {
+private class PocketVideoAdapter(
+        context: Context,
+        feedItemsDeferred: PocketVideosDeferred
+) : RecyclerView.Adapter<PocketVideoViewHolder>() {
 
     private val photonGrey70 = ContextCompat.getColor(context, R.color.photonGrey70)
     private val photonGrey60 = ContextCompat.getColor(context, R.color.photonGrey60)
@@ -48,8 +91,11 @@ private class PocketVideoAdapter(context: Context) : RecyclerView.Adapter<Pocket
     private val videoItemHorizontalMargin = context.resources.getDimensionPixelSize(R.dimen.pocket_video_item_horizontal_margin)
     private val feedHorizontalMargin = context.resources.getDimensionPixelSize(R.dimen.pocket_feed_horizontal_margin)
 
-    private val feedItems = List(8) {
-        PocketVideo("Mirror-Polished Japanese $it Ball Challenge Crushed", "youtube.com/tv", "youtube.com/tv", "youtube.com/tv")
+    private var feedItems: List<PocketVideo> = if (!feedItemsDeferred.isCompleted) {
+        // We let the parent wait for this to complete to simplify the code here.
+        throw IllegalStateException("Expected deferred to be completed in parent before creation")
+    } else {
+        feedItemsDeferred.getCompleted() ?: emptyList()
     }
 
     override fun getItemCount() = feedItems.size
