f188eb91d16858cf82043379f6c552358248f280
Michael Comella <michael.l.comella@gmail.com>
2018-05-01 14:11:57 -0700

    Issue #774: Add RoundCornerTransformation.
    This lets us easily round image corners during a Picasso load.

--- app/src/main/java/org/mozilla/focus/utils/PicassoWrapper.kt
+++ app/src/main/java/org/mozilla/focus/utils/PicassoWrapper.kt
@@ -4,7 +4,10 @@
 
 package org.mozilla.focus.utils
 
+import android.graphics.Bitmap
 import com.squareup.picasso.Picasso
+import com.squareup.picasso.Transformation
+import org.mozilla.focus.ext.withRoundedCorners
 
 /**
  * A holder for the shared Picasso instance. All calls to Picasso should go through this class.
@@ -12,4 +15,23 @@ import com.squareup.picasso.Picasso
 object PicassoWrapper {
     @JvmStatic
     val client get() = Picasso.get()
+}
+
+/**
+ * Rounds the corners of the given Bitmap. This method will not work correctly if the Bitmap
+ * content is skewed or cropped.
+ *
+ * This method isn't efficient: it creates a copy of the bitmap you're trying to round the
+ * corners of. A more efficient method would be to create a Drawable or ImageView that draws
+ * the Bitmap without having to modify it, but we don't have one of those implementations yet.
+ */
+class RoundCornerTransformation(private val radiusPx: Float) : Transformation {
+
+    override fun transform(source: Bitmap): Bitmap {
+        val roundedBitmap = source.withRoundedCorners(radiusPx)
+        source.recycle()
+        return roundedBitmap
+    }
+
+    override fun key(): String = "RoundCornerTransformation-$radiusPx"
 }
