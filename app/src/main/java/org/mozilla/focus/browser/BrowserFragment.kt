a411de66bfff605db5b80377b2198d58f4cd0b12
Michael Comella <michael.l.comella@gmail.com>
2018-06-20 15:34:23 -0700

    Issue #930, Closes #936: Add play/pause voice command support.
    For time's sake, we:
    - Only do play/pause
    - Do not handle audio (b/c mute key events, see below)
    
    After this change, we handle play/pause events ourselves (through
    MediaSession) and mute these key events from reaching the web page (who
    could have handled these themselves, such as youtube). Benefits:
    - Voice and hardware remotes use the same code path for the same actions
    - We can play/pause videos on pages that did not previously support it
    
    Downside is that pages may have extra special handling of the media
    buttons that we're disabling, but we have yet to see this.

--- app/src/main/java/org/mozilla/focus/browser/BrowserFragment.kt
+++ app/src/main/java/org/mozilla/focus/browser/BrowserFragment.kt
@@ -69,6 +69,8 @@ class BrowserFragment : IWebViewLifecycleFragment() {
     override val initialUrl get() = session.url.value
     override val iWebViewCallback get() = SessionCallbackProxy(session, BrowserIWebViewCallback(this))
 
+    private lateinit var videoVoiceCommandMediaSession: VideoVoiceCommandMediaSession
+
     /**
      * The current URL.
      *
@@ -95,6 +97,7 @@ class BrowserFragment : IWebViewLifecycleFragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initSession()
+        initMediaSession()
     }
 
     private fun initSession() {
@@ -115,6 +118,11 @@ class BrowserFragment : IWebViewLifecycleFragment() {
                 }
             }
         })
+    }
+
+    private fun initMediaSession() {
+        videoVoiceCommandMediaSession = VideoVoiceCommandMediaSession(activity!!)
+        lifecycle.addObserver(videoVoiceCommandMediaSession)
     }
 
     private val onNavigationEvent = { event: NavigationEvent, value: String?,
@@ -165,6 +173,8 @@ class BrowserFragment : IWebViewLifecycleFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val layout = inflater.inflate(R.layout.fragment_browser, container, false)
 
+        videoVoiceCommandMediaSession.iWebView = layout.webview
+
         cursor = CursorController(this, cursorParent = layout.browserFragmentRoot,
                 view = layout.cursorView)
         lifecycle.addObserver(cursor!!)
@@ -210,6 +220,9 @@ class BrowserFragment : IWebViewLifecycleFragment() {
 
     override fun onDestroyView() {
         super.onDestroyView()
+
+        videoVoiceCommandMediaSession.iWebView = null
+
         lifecycle.removeObserver(cursor!!)
         cursor = null
         overlayVisibleCached = browserOverlay.visibility
@@ -249,12 +262,14 @@ class BrowserFragment : IWebViewLifecycleFragment() {
     fun dispatchKeyEvent(event: KeyEvent): Boolean {
         /**
          * Key handling order:
+         * - MediaSession overrides
          * - Menu to control overlay
          * - Youtube remap of BACK to ESC
          * - Cursor
          * - Return false, as unhandled
          */
-        return (handleSpecialKeyEvent(event)) ||
+        return videoVoiceCommandMediaSession.dispatchKeyEvent(event) ||
+                handleSpecialKeyEvent(event) ||
                 (cursor?.keyDispatcher?.dispatchKeyEvent(event) ?: false)
     }
 
