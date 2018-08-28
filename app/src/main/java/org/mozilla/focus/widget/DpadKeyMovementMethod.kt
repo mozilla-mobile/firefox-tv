bf0eb283c21fc7aada91ecfe8161989ae3cda779
Simon Chae <chaesmn@gmail.com>
2018-08-28 15:49:19 -0400

    Closes #995: Remove redundant code, add java doc

--- app/src/main/java/org/mozilla/focus/widget/DpadKeyMovementMethod.kt
+++ app/src/main/java/org/mozilla/focus/widget/DpadKeyMovementMethod.kt
@@ -5,20 +5,13 @@
 
 package org.mozilla.focus.widget
 
-import android.text.Selection
 import android.text.Spannable
 import android.text.method.ArrowKeyMovementMethod
-import android.view.View
 import android.widget.TextView
 
+/**
+ * An ArrowKeyMovementMethod that eliminates end cursor selection onFocus
+ */
 class DpadKeyMovementMethod : ArrowKeyMovementMethod() {
-
-    override fun onTakeFocus(view: TextView?, text: Spannable?, dir: Int) {
-        if (dir and (View.FOCUS_FORWARD or View.FOCUS_DOWN) != 0) {
-            if (view?.layout == null) {
-                // This shouldn't be null, but do something sensible if it is.
-                Selection.setSelection(text, text!!.length)
-            }
-        }
-    }
+    override fun onTakeFocus(view: TextView, text: Spannable, dir: Int) {}
 }
