9a1fc4ca7c3583889e86b143454082671f142f41
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2018-09-17 21:29:28 +0530

    No issue: Add unit tests for Context ext function

---
+++ app/src/test/java/org/mozilla/focus/ext/ContextTest.kt
@@ -0,0 +1,53 @@
+package org.mozilla.focus.ext
+
+import android.content.Context
+import android.view.accessibility.AccessibilityManager
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows.shadowOf
+import org.robolectric.util.ReflectionHelpers
+
+@RunWith(RobolectricTestRunner::class)
+class ContextTest {
+
+    private var context: Context? = null
+    private var accessibilityManager: AccessibilityManager? = null
+
+    @Before
+    fun startUp() {
+        context = RuntimeEnvironment.application
+        accessibilityManager = context!!.getAccessibilityManager()
+    }
+
+    @Test
+    fun `isScreenReaderEnabled() returns true when TouchExploration enabled`() {
+        shadowOf(accessibilityManager).isTouchExplorationEnabled = true
+        assertTrue(context!!.isVoiceViewEnabled())
+    }
+    @Test
+    fun `isScreenReaderEnabled() returns false when TouchExploration disabled`() {
+        shadowOf(accessibilityManager).isTouchExplorationEnabled = false
+        assertFalse(context!!.isVoiceViewEnabled())
+    }
+
+    @Test
+    fun `getAccessibilityManager() provides a working instance`() {
+        shadowOf(accessibilityManager).isEnabled = true
+        assertTrue(accessibilityManager!!.isEnabled)
+        assertTrue(getAccessibilityManagerInstance().isEnabled)
+        shadowOf(accessibilityManager).isEnabled = false
+        assertFalse(accessibilityManager!!.isEnabled)
+        assertFalse(getAccessibilityManagerInstance().isEnabled)
+    }
+
+    @Throws(Exception::class)
+    private fun getAccessibilityManagerInstance(): AccessibilityManager {
+        return ReflectionHelpers.callStaticMethod(AccessibilityManager::class.java, "getInstance",
+                ReflectionHelpers.ClassParameter.from(Context::class.java, RuntimeEnvironment.application))
+    }
+}
