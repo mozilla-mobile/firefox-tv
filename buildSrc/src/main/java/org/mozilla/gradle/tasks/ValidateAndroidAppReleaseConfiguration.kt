358c817eb71c560cf90e6449a626d559c6a7664b
Michael Comella <michael.l.comella@gmail.com>
2018-10-13 08:44:54 -0700

    Issue #1280: Add ValidateAndroidAppReleaseConfiguration task.

---
+++ buildSrc/src/main/java/org/mozilla/gradle/tasks/ValidateAndroidAppReleaseConfiguration.kt
@@ -0,0 +1,83 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.gradle.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+import org.mozilla.gradle.GitAggregates
+import org.mozilla.gradle.ext.androidDSLOrThrow
+
+/**
+ * Validates that an Android app is correctly configured for release. When created, this
+ * task will automatically create dependencies upon the release tasks.
+ *
+ * This task currently asserts:
+ * - There are no uncommitted changes in git
+ * - The buildVersion in gradle matches the checked out git tag
+ *
+ * This task can be skipped by supplying the `-Pno-validate` argument.
+ */
+open class ValidateAndroidAppReleaseConfiguration : DefaultTask() {
+    init {
+        group = "Verification"
+        description = "Validates an Android app is correctly configured for release"
+
+        this.onlyIf { !project.hasProperty("no-validate") }
+
+        project.gradle.projectsEvaluated {
+            getAssembleReleaseTasks().forEach {
+                it.dependsOn(this@ValidateAndroidAppReleaseConfiguration)
+            }
+
+            // The assemble release tasks run late during execution so we make
+            getCompileReleaseTasks().forEach {
+                it.mustRunAfter(this@ValidateAndroidAppReleaseConfiguration)
+            }
+        }
+    }
+
+    private fun getAssembleReleaseTasks() = project.tasks.filter { task -> task.name.let {
+        it.startsWith("assemble") && it.endsWith("Release")
+    } }
+
+    private fun getCompileReleaseTasks() = project.tasks.filter { task -> task.name.let {
+        it.startsWith("compile") && it.endsWith("ReleaseSources")
+    } }
+
+    @TaskAction
+    fun validateAndroidAppRelease() {
+        // If you update which validations run, update the class kdoc too.
+        validateNoUncommittedChanges()
+        validateVersions()
+    }
+
+    private fun validateNoUncommittedChanges() {
+        if (GitAggregates.hasUncommittedChanges()) {
+            throw IllegalStateException("Unable to build release builds with uncommitted changes")
+        }
+    }
+
+    // May not work correctly if there are multiple git tags.
+    // In practice, this is uncommon for app releases.
+    @Suppress("ThrowsCount") // Throwing to stop gradle builds is necessary.
+    private fun validateVersions() {
+        fun getGitTagVersionName(): String {
+            // Expected: "v1.1"
+            val gitTag = GitAggregates.getCheckedOutGitTag()
+                    ?: throw IllegalStateException("Expected top-most commit to have a git tag")
+            if (gitTag.getOrNull(0) != 'v') {
+                throw IllegalStateException("Expected git tag to be a version, e.g. 'v1.1'")
+            }
+            return gitTag.drop(1) // remove "v"
+        }
+
+        val buildVersionName = project.androidDSLOrThrow().extension.defaultConfig.versionName
+        val gitTagVersionName = getGitTagVersionName()
+        if (buildVersionName != gitTagVersionName) {
+            throw IllegalStateException("Expected build.gradle versionName, $buildVersionName, " +
+                    "to match git tag version name, $gitTagVersionName")
+        }
+    }
+}
