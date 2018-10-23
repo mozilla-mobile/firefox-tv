1d2fd5a03da64e59a9abb3066db77cedab02dbae
Michael Comella <michael.l.comella@gmail.com>
2018-10-23 10:05:50 -0700

    Issue #1327: Add isEnabled(boolean) matcher.
    Hamcrest + android matchers force us into a `isEnabled()` and
    `not(isEnabled())` pattern. These matchers let us break these patterns
    to use `isEnabled(boolean)` instead.

---
+++ app/src/androidTest/java/org/mozilla/tv/firefox/helpers/Matchers.kt
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.helpers
+
+import android.support.test.espresso.matcher.ViewMatchers.isEnabled as espressoIsEnabled
+import android.view.View
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matcher
+
+/**
+ * The [android.support.test.espresso.matcher.ViewMatchers.isEnabled] function that can also handle
+ * disabled state through the boolean argument.
+ */
+fun isEnabled(isEnabled: Boolean): Matcher<View> = if (isEnabled) {
+    espressoIsEnabled()
+} else {
+    not(espressoIsEnabled())
+}
