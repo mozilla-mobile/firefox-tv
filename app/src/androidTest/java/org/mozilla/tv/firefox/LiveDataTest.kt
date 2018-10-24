e5d16cd448624a50ac6e4b9bdc671c7dd1d0ba7e
Severin Rudie <srudie@mozilla.com>
2018-10-23 17:21:12 -0700

    Issue #1352: adds LiveData and related tests

---
+++ app/src/androidTest/java/org/mozilla/tv/firefox/LiveDataTest.kt
@@ -0,0 +1,93 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.tv.firefox.helpers.SkipOnboardingMainActivityTestRule
+import org.mozilla.tv.firefox.utils.LiveData
+
+@RunWith(AndroidJUnit4::class)
+@Suppress("TestFunctionName")
+// Espresso test functions can't have spaces in their names, even between backticks. This prevents
+// the linting error for starting a function with an uppercase letter
+class LiveDataTest {
+
+    @Rule @JvmField
+    val activityTestRule = SkipOnboardingMainActivityTestRule()
+
+    private lateinit var source1: MutableLiveData<Int>
+    private lateinit var source2: MutableLiveData<String>
+    private lateinit var result: MutableLiveData<Pair<Int, String>>
+
+    @Before
+    fun setup() {
+        source1 = MutableLiveData()
+        source2 = MutableLiveData()
+
+        result = LiveData.combineLatest(source1, source2) { v1, v2 -> Pair(v1, v2) } as MutableLiveData<Pair<Int, String>>
+    }
+
+    @Test
+    fun WHEN_the_first_source_never_emits_THEN_the_result_should_never_emit() {
+        activityTestRule.runOnUiThread {
+            result.observeForever { fail("Shouldn't emit anything") }
+
+            source2.value = "A"
+            source2.value = "B"
+            source2.value = "C"
+        }
+    }
+
+    @Test
+    fun WHEN_the_second_source_never_emits_THEN_the_result_should_never_emit() {
+        activityTestRule.runOnUiThread {
+            result.observeForever { fail("Shouldn't emit anything") }
+
+            source1.value = 1
+            source1.value = 2
+            source1.value = 3
+        }
+    }
+
+    @Test
+    fun WHEN_both_sources_emit_THEN_the_result_should_emit_pairs() {
+        activityTestRule.runOnUiThread {
+            var observer = Observer<Pair<Int, String>> { assertEquals(Pair(1, "A"), it) }
+
+            result.observeForever(observer)
+
+            source1.value = 1
+            source2.value = "A"
+
+            result.removeObserver(observer)
+
+            source1.value = 2
+            source2.value = "B"
+
+            observer = Observer { assertEquals(Pair(2, "B"), it) }
+            result.observeForever(observer)
+        }
+    }
+
+    @Test
+    fun GIVEN_a_value_has_already_been_emitted_WHEN_only_one_source_emits_THEN_the_result_should_use_cached_value() {
+        activityTestRule.runOnUiThread {
+            source1.value = 1
+            source2.value = "A"
+
+            source1.value = 2
+
+            result.observeForever { assertEquals(Pair(2, "A"), it) }
+        }
+    }
+}
---
+++ app/src/main/java/org/mozilla/tv/firefox/utils/LiveData.kt
@@ -0,0 +1,32 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.utils
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MediatorLiveData
+
+object LiveData {
+
+    // If more advanced versions of this are required, we should import RxJava instead of duplicating their effort
+    fun <T, U, R> combineLatest(
+        source1: LiveData<T>,
+        source2: LiveData<U>,
+        combiner: (T, U) -> R
+    ): LiveData<R> {
+        val result = MediatorLiveData<R>()
+
+        val merge = merge@{
+            val source1Value = source1.value ?: return@merge
+            val source2Value = source2.value ?: return@merge
+
+            result.value = combiner(source1Value, source2Value)
+        }
+
+        result.addSource(source1) { merge.invoke() }
+        result.addSource(source2) { merge.invoke() }
+
+        return result
+    }
+}
