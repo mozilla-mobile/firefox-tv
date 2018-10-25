1e5936bc87f18bf9263013d0d234f585c34a586c
Severin Rudie <srudie@mozilla.com>
2018-10-25 15:19:25 -0700

    Issue #1344: extracted fun into PocketVideoParser

---
+++ app/src/main/java/org/mozilla/tv/firefox/pocket/PocketVideoParser.kt
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.pocket
+
+import org.json.JSONException
+import org.json.JSONObject
+
+/**
+ * Handles marshalling [PocketFeedItem.Video] objects from JSON.
+ */
+object PocketVideoParser {
+
+    fun parse(jsonObj: JSONObject): PocketFeedItem.Video? = try {
+        PocketFeedItem.Video(
+            id = jsonObj.getInt("id"),
+            title = jsonObj.getString("title"),
+            // Note that the 'url' property of our domain object can be retrieved from
+            // either of two JSON fields, and we make no distinction internally
+            url = jsonObj.optString("tv_url", null) ?: jsonObj.getString("url"),
+            thumbnailURL = jsonObj.getString("image_src"),
+            popularitySortId = jsonObj.getInt("popularity_sort_id")
+        )
+    } catch (e: JSONException) {
+        null
+    }
+}
