e0132a115237e4faa7800e0e6d06247c2d7a753e
Severin Rudie <srudie@mozilla.com>
2018-10-25 14:55:34 -0700

    Issue #1344: added static data to PocketViewModel

--- app/src/main/java/org/mozilla/tv/firefox/pocket/PocketViewModel.kt
+++ app/src/main/java/org/mozilla/tv/firefox/pocket/PocketViewModel.kt
@@ -6,6 +6,10 @@ package org.mozilla.tv.firefox.pocket
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.annotation.VisibleForTesting
+import org.mozilla.tv.firefox.R
+
+const val POCKET_VIDEO_COUNT = 20
 
 sealed class PocketViewModelState {
     object Error : PocketViewModelState()
@@ -35,4 +39,17 @@ class PocketViewModel(pocketRepo: PocketRepo) : ViewModel() {
 
     val state = MutableLiveData<PocketViewModelState>()
 
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    val loadingPlaceholders: List<PocketFeedItem> =
+        List(POCKET_VIDEO_COUNT) { PocketFeedItem.Loading(R.color.photonGrey50) }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    val noKeyPlaceholders: List<PocketFeedItem> = (1..POCKET_VIDEO_COUNT).map { PocketFeedItem.Video(
+        id = it,
+        title = "Mozilla",
+        url = "https://www.mozilla.org/en-US/",
+        thumbnailURL = "https://blog.mozilla.org/firefox/files/2017/12/Screen-Shot-2017-12-18-at-2.39.25-PM.png",
+        popularitySortId = it
+        )
+    }
 }
