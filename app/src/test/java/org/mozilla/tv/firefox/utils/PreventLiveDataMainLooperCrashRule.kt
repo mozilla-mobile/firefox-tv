603ba03d81fa43a2f585357352d0d8a499bb5486
Severin Rudie <srudie@mozilla.com>
2018-10-29 11:27:43 -0700

    Closes #1344: add missing kdocs

--- app/src/test/java/org/mozilla/tv/firefox/utils/PreventLiveDataMainLooperCrashRule.kt
+++ app/src/test/java/org/mozilla/tv/firefox/utils/PreventLiveDataMainLooperCrashRule.kt
@@ -5,8 +5,15 @@
 package org.mozilla.tv.firefox.utils
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.LiveData
 
 /**
- * TODO
+ * This rule prevents a crash when unit testing [LiveData].
+ *
+ * Internally, [LiveData] adds some runnables to the main Android event loop,
+ * which does not exist on the JVM.  With this rule active, these are run
+ * immediately instead.
+ *
+ * See: https://medium.com/pxhouse/unit-testing-with-mutablelivedata-22b3283a7819
  */
 typealias PreventLiveDataMainLooperCrashRule = InstantTaskExecutorRule
