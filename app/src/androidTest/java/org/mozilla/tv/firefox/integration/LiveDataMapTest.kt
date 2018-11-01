180659ae6495a8402136a3490cb61a6e6ba8ff6d
Michael Comella <michael.l.comella@gmail.com>
2018-11-01 12:21:38 -0700

    Issue #1441: Move LiveData tests to integration pkg.
    The helpers package is for testing support code, not tests for support
    code: all tests should go in integration/ or ui/.

--- app/src/androidTest/java/org/mozilla/tv/firefox/integration/LiveDataMapTest.kt
+++ app/src/androidTest/java/org/mozilla/tv/firefox/integration/LiveDataMapTest.kt
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla.tv.firefox.helpers.ext
+package org.mozilla.tv.firefox.integration
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
