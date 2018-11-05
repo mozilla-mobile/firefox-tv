62bedc189b6f40068cb9371f536938ffe16322f2
Severin Rudie <srudie@mozilla.com>
2018-11-05 13:39:43 -0800

    Closes #1045: wrote test to cover Pocket happy path

--- app/src/androidTest/java/org/mozilla/tv/firefox/FirefoxTestApplication.kt
+++ app/src/androidTest/java/org/mozilla/tv/firefox/FirefoxTestApplication.kt
@@ -2,11 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package org.mozilla
+package org.mozilla.tv.firefox
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
-import org.mozilla.tv.firefox.FirefoxApplication
 import org.mozilla.tv.firefox.pocket.PocketEndpoint
 import org.mozilla.tv.firefox.pocket.PocketFeedStateMachine
 import org.mozilla.tv.firefox.pocket.PocketVideoRepo
@@ -14,12 +13,11 @@ import org.mozilla.tv.firefox.pocket.PocketViewModel
 import org.mozilla.tv.firefox.utils.BuildConfigDerivables
 import org.mozilla.tv.firefox.utils.ServiceLocator
 
-@Suppress("UNCHECKED_CAST")
 class FirefoxTestApplication : FirefoxApplication() {
 
     private val pocketEndpoint = object : PocketEndpoint() {
         override suspend fun getRecommendedVideos(): List<PocketViewModel.FeedItem.Video>? {
-            return PocketViewModel.noKeyPlaceholders as List<PocketViewModel.FeedItem.Video>
+            return PocketViewModel.noKeyPlaceholders
         }
     }
 
