18f3ed4cdcc8469809608f624e03bac84c5c7171
Severin Rudie <srudie@mozilla.com>
2018-11-06 14:54:33 -0800

    Issue #1392: write SessionRepo skeleton

---
+++ app/src/main/java/org/mozilla/tv/firefox/session/SessionRepo.kt
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.session
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.support.annotation.AnyThread
+import mozilla.components.browser.session.SessionManager
+import org.mozilla.tv.firefox.ext.postIfNew
+
+/**
+ * TODO
+ */
+class SessionRepo(private val sessionManager: SessionManager) {
+
+    data class State(
+        val backEnabled: Boolean,
+        val forwardEnabled: Boolean,
+        val desktopModeActive: Boolean,
+        val currentUrl: String
+    )
+
+    private val _state = MutableLiveData<State>()
+    val state: LiveData<State> = _state
+
+    @AnyThread
+    fun update() {
+        sessionManager.selectedSession?.let {
+            val newState = State(
+                backEnabled = it.canGoBack,
+                forwardEnabled = it.canGoForward,
+                desktopModeActive = it.desktopMode,
+                currentUrl = it.url
+            )
+            _state.postIfNew(newState)
+        }
+    }
+}
