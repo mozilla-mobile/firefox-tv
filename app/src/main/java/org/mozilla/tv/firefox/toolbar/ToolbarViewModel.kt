4d6c3de8baa9ca1941ebda6717e3c74ef179edbb
Severin Rudie <srudie@mozilla.com>
2018-11-06 16:13:29 -0800

    Issue #1392: fleshed out ToolbarViewModel

--- app/src/main/java/org/mozilla/tv/firefox/toolbar/ToolbarViewModel.kt
+++ app/src/main/java/org/mozilla/tv/firefox/toolbar/ToolbarViewModel.kt
@@ -5,10 +5,19 @@
 package org.mozilla.tv.firefox.toolbar
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import org.mozilla.tv.firefox.pinnedtile.PinnedTileRepo
+import org.mozilla.tv.firefox.session.SessionRepo
+import org.mozilla.tv.firefox.ext.LiveDataHelper
+import org.mozilla.tv.firefox.utils.AppConstants
+import org.mozilla.tv.firefox.utils.TurboMode
+import org.mozilla.tv.firefox.utils.UrlUtils
 
-open class ToolbarViewModel : ViewModel() {
+open class ToolbarViewModel(
+    sessionRepo: SessionRepo,
+    pinnedTileRepo: PinnedTileRepo,
+    turboMode: TurboMode
+) : ViewModel() {
 
     data class State(
         val backEnabled: Boolean,
@@ -22,7 +31,22 @@ open class ToolbarViewModel : ViewModel() {
         val urlBarText: String
     )
 
-    private val _state = MutableLiveData<ToolbarViewModel.State>()
-    open val state: LiveData<ToolbarViewModel.State> = _state
+    val state: LiveData<ToolbarViewModel.State> =
+        LiveDataHelper.combineLatest(sessionRepo.state, pinnedTileRepo.getPinnedTiles()) { sessionState, pinnedTiles ->
 
+            fun isUrlEqualToHomepage() = sessionState.currentUrl == AppConstants.APP_URL_HOME
+            fun currentUrlIsPinned() = pinnedTiles.containsKey(sessionState.currentUrl)
+
+            ToolbarViewModel.State(
+                backEnabled = sessionState.backEnabled,
+                forwardEnabled = sessionState.forwardEnabled,
+                refreshEnabled = !isUrlEqualToHomepage(),
+                pinEnabled = !isUrlEqualToHomepage(),
+                pinChecked = currentUrlIsPinned(),
+                turboChecked = turboMode.isEnabled(),
+                desktopModeEnabled = !isUrlEqualToHomepage(),
+                desktopModeChecked = sessionState.desktopModeActive,
+                urlBarText = UrlUtils.toUrlBarDisplay(sessionState.currentUrl)
+            )
+        }
 }
