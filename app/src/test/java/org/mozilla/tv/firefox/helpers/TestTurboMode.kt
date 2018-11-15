338185fc5f4364bfd9a002cae4502d640ca9e7db
Severin Rudie <srudie@mozilla.com>
2018-11-15 14:26:29 -0800

    Issue #1392: added test implementation of TurboMode

---
+++ app/src/test/java/org/mozilla/tv/firefox/helpers/TestTurboMode.kt
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.helpers
+
+import org.mozilla.tv.firefox.utils.TurboMode
+
+/**
+ * An implementation of [TurboMode] that does not persist state between
+ * instances.
+ */
+class TestTurboMode(private var isEnabled: Boolean) : TurboMode {
+    override fun isEnabled(): Boolean = isEnabled
+
+    override fun toggle() {
+        isEnabled = !isEnabled
+    }
+}
