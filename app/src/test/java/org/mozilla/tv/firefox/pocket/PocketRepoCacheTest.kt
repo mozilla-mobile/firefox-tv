30f57dbbf5861830122497b44e53f78a579feb1d
Severin Rudie <srudie@mozilla.com>
2018-12-20 15:46:55 -0800

    Closes #1607: improved PocketRepoCacheTest

--- app/src/test/java/org/mozilla/tv/firefox/pocket/PocketRepoCacheTest.kt
+++ app/src/test/java/org/mozilla/tv/firefox/pocket/PocketRepoCacheTest.kt
@@ -7,7 +7,6 @@ package org.mozilla.tv.firefox.pocket
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
-import junit.framework.TestCase.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Rule
@@ -17,6 +16,7 @@ import org.mockito.Mockito.mock
 import org.mockito.Mockito.spy
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
+import org.mozilla.tv.firefox.helpers.ext.assertValues
 import org.mozilla.tv.firefox.utils.BuildConfigDerivables
 import org.mozilla.tv.firefox.utils.PreventLiveDataMainLooperCrashRule
 
@@ -46,61 +46,61 @@ class PocketRepoCacheTest {
         }
         repoCache = PocketRepoCache(repo)
         cacheOutput = repoCache.feedState
-        repoCache.frozen = false
+        repoCache.setup()
     }
 
     @Test
     fun `WHEN repo has not emitted anything THEN cache should not emit anything`() {
+        repoCache.frozen = false
+
         observerSpy = spy(Observer { })
         cacheOutput.observeForever(observerSpy)
         verify(observerSpy, times(0)).onChanged(any())
     }
 
     @Test
-    fun `WHEN repo emits non-load-complete values THEN cache should emit the same values`() {
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.FetchFailed, it) })
-        repoOutput.value = PocketVideoRepo.FeedState.FetchFailed
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
+    fun `GIVEN cache is unfrozen WHEN repo emits non-load-complete values THEN cache should emit the same values`() {
+        repoCache.frozen = false
 
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.Loading, it) })
-        repoOutput.value = PocketVideoRepo.FeedState.Loading
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
-
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.NoAPIKey, it) })
-        repoOutput.value = PocketVideoRepo.FeedState.NoAPIKey
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
+        repoCache.feedState.assertValues(
+            PocketVideoRepo.FeedState.FetchFailed,
+            PocketVideoRepo.FeedState.Loading,
+            PocketVideoRepo.FeedState.NoAPIKey
+        ) {
+            repoOutput.value = PocketVideoRepo.FeedState.FetchFailed
+            repoOutput.value = PocketVideoRepo.FeedState.Loading
+            repoOutput.value = PocketVideoRepo.FeedState.NoAPIKey
+        }
     }
 
     @Test
-    fun `GIVEN cache has received a value AND cache is not accepting updates WHEN future values are emitted from the repo THEN new values should not be emitted by the cache `() {
+    fun `GIVEN cache has received a valid value AND cache is frozen WHEN future values are emitted from the repo THEN new values should not be emitted by the cache `() {
         val firstVideos = listOf(PocketViewModel.FeedItem.Video(1, "", "", "", 1, ""))
         val secondVideos = listOf(PocketViewModel.FeedItem.Video(2, "", "", "", 2, ""))
         assertNotEquals(firstVideos, secondVideos)
 
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.LoadComplete(firstVideos), it) })
-        repoOutput.value = PocketVideoRepo.FeedState.LoadComplete(firstVideos)
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
+        repoCache.frozen = false
 
+        repoCache.feedState.assertValues(PocketVideoRepo.FeedState.LoadComplete(firstVideos)) {
+            repoOutput.value = PocketVideoRepo.FeedState.LoadComplete(firstVideos)
+            repoCache.frozen = true
+            repoOutput.value = PocketVideoRepo.FeedState.Loading
+            repoOutput.value = PocketVideoRepo.FeedState.LoadComplete(secondVideos)
+        }
+    }
+
+    @Test
+    fun `GIVEN cache has not received a valid value AND cache is frozen WHEN future values are emitted from the repo THEN new values should be emitted by the cache`() {
         repoCache.frozen = true
 
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.LoadComplete(firstVideos), it) })
-        repoOutput.value = PocketVideoRepo.FeedState.Loading
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
-
-        observerSpy = spy(Observer { assertEquals(PocketVideoRepo.FeedState.LoadComplete(firstVideos), it) })
-        repoOutput.value = PocketVideoRepo.FeedState.LoadComplete(secondVideos)
-        cacheOutput.observeForever(observerSpy)
-        verify(observerSpy, times(1)).onChanged(any())
-        cacheOutput.removeObserver(observerSpy)
+        repoCache.feedState.assertValues(
+            PocketVideoRepo.FeedState.FetchFailed,
+            PocketVideoRepo.FeedState.Loading,
+            PocketVideoRepo.FeedState.NoAPIKey
+        ) {
+            repoOutput.value = PocketVideoRepo.FeedState.FetchFailed
+            repoOutput.value = PocketVideoRepo.FeedState.Loading
+            repoOutput.value = PocketVideoRepo.FeedState.NoAPIKey
+        }
     }
 }
