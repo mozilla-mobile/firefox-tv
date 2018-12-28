42dd217e93de22e59e73880ea1e17611cea7f65d
Chenxia Liu <liuche@mozilla.com>
2018-12-28 11:55:08 -0800

    Closes #1393 - Address review comments

--- app/src/main/java/org/mozilla/tv/firefox/settings/SettingsRepo.kt
+++ app/src/main/java/org/mozilla/tv/firefox/settings/SettingsRepo.kt
@@ -12,9 +12,9 @@ import org.mozilla.tv.firefox.R
 private const val PREF_KEY_TELEMETRY = R.string.pref_key_telemetry
 const val IS_TELEMETRY_ENABLED_DEFAULT = true
 
-class SettingsRepo(private val applicationContext: Application) {
+class SettingsRepo(applicationContext: Application) {
     private val _sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-    private val _resources = applicationContext.resources
+    private val resources = applicationContext.resources
 
     private val _dataCollectionEnabled = MutableLiveData<Boolean>()
     val dataCollectionEnabled: LiveData<Boolean> = _dataCollectionEnabled
@@ -26,7 +26,6 @@ class SettingsRepo(private val applicationContext: Application) {
     private fun loadSettingsFromPreferences() {
         // The first access to shared preferences will require a disk read.
         StrictMode.allowThreadDiskReads().resetAfter {
-            val resources = applicationContext.resources
             _dataCollectionEnabled.value = _sharedPreferences
                     .getBoolean(resources.getString(PREF_KEY_TELEMETRY), IS_TELEMETRY_ENABLED_DEFAULT)
         }
@@ -34,7 +33,7 @@ class SettingsRepo(private val applicationContext: Application) {
 
     fun setDataCollectionEnabled(toEnable: Boolean) {
         _sharedPreferences.edit()
-                .putBoolean(_resources.getString(PREF_KEY_TELEMETRY), toEnable)
+                .putBoolean(resources.getString(PREF_KEY_TELEMETRY), toEnable)
                 .apply()
         _dataCollectionEnabled.value = toEnable
     }
