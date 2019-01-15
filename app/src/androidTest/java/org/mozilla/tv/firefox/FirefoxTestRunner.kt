1768c0f60d2e68286655127eb9da54b203a2e9ed
Severin Rudie <srudie@mozilla.com>
2019-01-14 17:07:32 -0800

    Closes #1651: adds Bundle.extractClass to FirefoxTestRunner

--- app/src/androidTest/java/org/mozilla/tv/firefox/FirefoxTestRunner.kt
+++ app/src/androidTest/java/org/mozilla/tv/firefox/FirefoxTestRunner.kt
@@ -6,6 +6,7 @@ package org.mozilla.tv.firefox
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
 import android.support.test.runner.AndroidJUnitRunner
 
 class FirefoxTestRunner : AndroidJUnitRunner() {
@@ -13,4 +14,16 @@ class FirefoxTestRunner : AndroidJUnitRunner() {
     override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
         return super.newApplication(cl, FirefoxTestApplication::class.java.name, context)
     }
+}
+
+/**
+ * Retrieves the class being tested from Espresso args
+ *
+ * Note that this is an implementation detail of Espresso, and could be a likely
+ * point of failure on version updates
+ */
+private fun Bundle?.extractClass(): String {
+    // The class is stored in the format of {class}#{test}
+    // e.g., org.mozilla.tv.firefox.ui.PocketBasicUserFlowTest#pocketBasicUserFlowTest
+    return this!!.getString("class")!!.split("#").first()
 }
