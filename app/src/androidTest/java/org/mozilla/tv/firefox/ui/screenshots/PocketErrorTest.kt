2c5ee3eef2e9159f39fb510a5f5261d0f0342895
Severin Rudie <srudie@mozilla.com>
2019-01-18 17:41:54 -0800

    Closes #1651: automatically convert PocketErrorTest to Kotlin

---
+++ app/src/androidTest/java/org/mozilla/tv/firefox/ui/screenshots/PocketErrorTest.kt
@@ -0,0 +1,68 @@
+/* -*- Mode: Java; c-basic-offset: 4; tab-width: 20; indent-tabs-mode: nil; -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.ui.screenshots
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ActivityTestRule
+import android.support.test.uiautomator.UiDevice
+import android.support.test.uiautomator.UiObject
+import android.support.test.uiautomator.UiSelector
+
+import org.junit.After
+import org.junit.ClassRule
+import org.junit.Rule
+import org.junit.Test
+import org.mozilla.tv.firefox.MainActivity
+import org.mozilla.tv.firefox.R
+import org.mozilla.tv.firefox.helpers.MainActivityTestRule
+import org.mozilla.tv.firefox.helpers.FakePocketVideoRepoProvider
+import org.mozilla.tv.firefox.pocket.PocketVideoRepo
+
+import tools.fastlane.screengrab.Screengrab
+import tools.fastlane.screengrab.locale.LocaleTestRule
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matchers.allOf
+
+class PocketErrorTest : ScreenshotTest() {
+
+    private val mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    @Rule
+    var mActivityTestRule: ActivityTestRule<MainActivity> = MainActivityTestRule()
+
+    @After
+    fun tearDown() {
+        mActivityTestRule.activity.finishAndRemoveTask()
+    }
+
+    @Test
+    fun showPocketTileError() {
+        FakePocketVideoRepoProvider.fakedPocketRepoState.postValue(PocketVideoRepo.FeedState.FetchFailed)
+
+        val errorMsg = mDevice.findObject(
+            UiSelector()
+                .resourceId("org.mozilla.tv.firefox.debug:id/pocketMegaTileLoadError")
+                .enabled(true)
+        )
+
+        errorMsg.waitForExists(5000)
+
+        Screengrab.screenshot("pocket-tile-error")
+
+        onView(allOf<View>(withId(R.id.megaTileTryAgainButton), isDisplayed()))
+            .perform(click())
+    }
+
+    companion object {
+
+        @ClassRule
+        val localeTestRule = LocaleTestRule()
+    }
+}
