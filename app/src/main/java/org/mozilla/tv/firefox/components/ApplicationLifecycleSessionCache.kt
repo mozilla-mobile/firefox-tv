d4462bfb1a8d7f6d448b31365ceb820c09ed75b9
Michael Comella <michael.l.comella@gmail.com>
2019-02-01 14:34:23 -0800

    Closes #1761, Issue #1628: Capture Sentry error if memory leak persists.

--- app/src/main/java/org/mozilla/tv/firefox/components/ApplicationLifecycleSessionCache.kt
+++ app/src/main/java/org/mozilla/tv/firefox/components/ApplicationLifecycleSessionCache.kt
@@ -8,6 +8,7 @@ import android.support.annotation.VisibleForTesting
 import android.support.annotation.VisibleForTesting.PRIVATE
 import mozilla.components.browser.session.Session
 import mozilla.components.browser.session.SessionManager
+import org.mozilla.tv.firefox.telemetry.SentryIntegration
 
 /**
  * An object that caches [Session]s in place of the [SessionManager] after the
@@ -34,6 +35,21 @@ class ApplicationLifecycleSessionCache(
      * the Application remains: this must be called before the Activity attempts to access the current session.
      */
     fun onCreateMaybeRestoreSessions() {
+        fun sendTelemetryIfMemoryLeakPersists() {
+            // If the SessionManager has Sessions when the app starts, they'll have references to a WebView: a memory
+            // leak. This will happen if the Activity is destroyed and recreated without onDestroy being called and
+            // the process/Application is not killed. With our understanding of Android, this should not happen but
+            // onDestroy is notoriously unreliable, the Android docs are not explicit about this, and we're running on
+            // non-standard Android. To be safe, we verify our assumptions with a telemetry ping. If this fails, we
+            // likely have to move the calls to onCreate/onStart/onStop; we chose not to write the code this way
+            // initially to keep thing simple.
+            if (sessionManager.size > 0) {
+                SentryIntegration.capture(IllegalStateException(
+                    "MEMORY LEAK: expected SessionManager to be empty in onCreate"))
+            }
+        }
+
+        sendTelemetryIfMemoryLeakPersists()
         if (cachedSessions.isEmpty()) return // unnecessary check but helps clarity.
 
         cachedSessions.forEach {
