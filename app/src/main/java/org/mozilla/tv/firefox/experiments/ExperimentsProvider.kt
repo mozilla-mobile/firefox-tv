323823d7bc278f1bc9ea7df41aa77c379e6eb0fb
Simon Chae <chaesmn@gmail.com>
2019-02-04 11:48:30 -0500

    Issue #1675: Add an ExperimentIllegalStateException

--- app/src/main/java/org/mozilla/tv/firefox/experiments/ExperimentsProvider.kt
+++ app/src/main/java/org/mozilla/tv/firefox/experiments/ExperimentsProvider.kt
@@ -5,6 +5,7 @@
 package org.mozilla.tv.firefox.experiments
 
 import android.content.Context
+import io.sentry.Sentry
 import mozilla.components.service.fretboard.ExperimentDescriptor
 import mozilla.components.service.fretboard.Fretboard
 import org.mozilla.tv.firefox.R
@@ -13,9 +14,9 @@ import org.mozilla.tv.firefox.R
  * [ExperimentsProvider] checks for experiment branch from [Fretboard] to provide its respective content.
  * See [getAAExitButtonExperiment] for example
  *
- * Note: Consider implementing fallback options since fretboard doesn't necessarily load the
- * latest changes from Kinto backend. See [FretboardProvider.updateExperiments] and
- * [FretboardProvider.loadExperiments] for more details
+ * Note: Consider implementing fallback options (log in Sentry using [ExperimentIllegalStateException]
+ * since fretboard doesn't necessarily load the latest changes from Kinto backend. See
+ * [FretboardProvider.updateExperiments] and [FretboardProvider.loadExperiments] for more details
  */
 class ExperimentsProvider(private val fretboard: Fretboard, private val context: Context) {
 
@@ -30,11 +31,14 @@ class ExperimentsProvider(private val fretboard: Fretboard, private val context:
                     context.resources.getString(R.string.exit_firefox_a11y,
                         context.resources.getString(R.string.firefox_tv_brand_name_short))
                 // Fallback: AA testing should be 50/50 so technically should never get to else.
-                else -> context.resources.getString(R.string.exit_firefox_a11y,
+                else -> {
+                    Sentry.capture(ExperimentIllegalStateException("AATestIllegalBranchName"))
+                    context.resources.getString(R.string.exit_firefox_a11y,
                         context.resources.getString(R.string.firefox_tv_brand_name_short))
+                }
             }
         } else {
-            // Fallback: AA testing should be 50/50 so technically should never get to else.
+            // The user is currently not part of the experiment
             context.resources.getString(R.string.exit_firefox_a11y,
                 context.resources.getString(R.string.firefox_tv_brand_name_short))
         }
