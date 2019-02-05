4b4fb9ac34958ff65649b49807a62f7bd24df56a
No Jun Park <npark@mozilla.com>
2019-02-05 15:33:06 -0500

    This commit addresses #1716 (the Firebase portion) and #1800 On each PR request, Taskcluster will download gcloud SDK, and uploads UI tests to Firebase. the test results, including the video capture, will be stored in the test-artifacts folder in the TC artifacts. The gcloud log will also provide an URL to view the test details as well.

---
+++ tools/taskcluster/execute-firebase-test.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# This script installs google cloud sdk, logs into google firebase, executes tests,
+# and collects test artifacts into the test_artifacts folder
+
+# If a command fails then do not proceed and fail this script too.
+set -ex
+#########################
+# The command line help #
+#########################
+display_help() {
+    echo "Usage: $0 Build_Variant Apk_Name Device_Config [Device_Config...]"
+    echo
+    echo "Do not put .apk prefix in Apk_Name"
+    echo "Device_Config should be in landscape mode (e.g. model=taimen,version=26,orientation=landscape)"
+    echo "Example: execute-firebase-test.sh debug app-debug model=taimen,version=26,orientation=landscape"
+    echo
+}
+
+# Basic parameter check
+if [[ $# -lt 3 ]]; then
+    echo "Your command line contains $# arguments"
+    display_help
+    exit 1
+fi
+
+# Get test configuration
+config_count=`expr $# - 2`
+config_param="--device $3"
+for ((i=4; i <= $#; i++))
+{
+    config_param+=" --device ${!i}"
+}
+
+# From now on disable exiting on error. If the tests fail we want to continue
+# and try to download the artifacts. We will exit with the actual error code later.
+set +e
+
+# Execute test set
+./google-cloud-sdk/bin/gcloud --format="json" firebase test android run \
+--type instrumentation \
+--app ./app/build/outputs/apk/$1/$2.apk \
+--test ./app/build/outputs/apk/androidTest/$1/$2-androidTest.apk \
+--use-orchestrator \
+--environment-variables clearPackageData=true \
+--results-bucket firefox-tv_test_artifacts \
+--timeout 30m \
+--no-auto-google-login $config_param
+
+exitcode=$?
+
+echo "Downloading artifacts"
+
+mkdir test_artifacts
+./google-cloud-sdk/bin/gsutil ls gs://firefox-tv_test_artifacts | tail -1 | ./google-cloud-sdk/bin/gsutil -m cp -r -I ./test_artifacts
+
+# Now exit the script with the exit code from the test run. (Only 0 if all test executions passed)
+exit $exitcode
--- tools/taskcluster/get-firebase-token.py
+++ tools/taskcluster/get-firebase-token.py
@@ -4,19 +4,19 @@
 
 """
 This script talks to the taskcluster secrets service to obtain the
-Pocket token and write it to the .pocket_key_release file in the root
-directory.
+Google Firebase service account token and write it to the .firebase_token
+file in the root directory.
 """
 
 import os
 import taskcluster
+import json
 
 # Get JSON data from taskcluster secrets service
 secrets = taskcluster.Secrets({'baseUrl': 'http://taskcluster/secrets/v1'})
 data = secrets.get('project/mobile/firefox-tv/tokens')
 
-token_file_path = os.path.join(os.path.dirname(__file__), '../../.pocket_key_release')
-with open(token_file_path, 'w') as token_file:
-	token_file.write(data['secret']['pocketToken'])
+with open(os.path.join(os.path.dirname(__file__), '../../.firebase_token.json'), 'w') as file:
+    json.dump(data['secret']['firebaseToken'], file)
 
-print("Imported pocket token from secrets service")
+print("Imported google firebase token from secrets service.")
