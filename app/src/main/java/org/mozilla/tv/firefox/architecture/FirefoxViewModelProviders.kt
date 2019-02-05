6f72ae9ea9cd1411e7e2b5ef2e7e518e03be4990
Michael Comella <michael.l.comella@gmail.com>
2019-02-04 16:46:14 -0800

    Issue #1787: Add FirefoxViewModelProviders, tests.
    This is taken roughly from FFES commit
    a38ec34d47c866d15b175b0d48203ed170b2e907.

---
+++ app/src/main/java/org/mozilla/tv/firefox/architecture/FirefoxViewModelProviders.kt
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.architecture
+
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+import org.mozilla.tv.firefox.ext.serviceLocator
+
+/**
+ * Returns an instance of the view model for the given application building block (fragment, activity)
+ * for the given lifecycle. This is a wrapper around the framework's [ViewModelProvider] using the application's
+ * [ViewModelFactory] to reduce code duplication.
+ */
+object FirefoxViewModelProviders {
+
+    fun of(activity: FragmentActivity): ViewModelProvider {
+        return ViewModelProviders.of(activity, activity.serviceLocator.viewModelFactory)
+    }
+
+    fun of(fragment: Fragment): ViewModelProvider {
+        // If we're attempting to retrieve a view model, we should be attached to a context already.
+        return ViewModelProviders.of(fragment, fragment.context!!.serviceLocator.viewModelFactory)
+    }
+}
