60a72823aa6e16e42680320f33c4eafa8053f367
Simon Chae <chaesmn@gmail.com>
2019-02-13 09:01:29 -0500

    Issue #1820: Add validateMotionEvent

--- app/src/main/java/org/mozilla/tv/firefox/webrender/cursor/CursorViewModel.kt
+++ app/src/main/java/org/mozilla/tv/firefox/webrender/cursor/CursorViewModel.kt
@@ -9,11 +9,16 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.graphics.PointF
 import android.os.SystemClock
+import android.support.annotation.VisibleForTesting
+import android.util.Log
+import android.view.KeyEvent
 import android.view.MotionEvent
+import android.view.ViewConfiguration
 import mozilla.components.support.base.observer.Consumable
 import org.mozilla.tv.firefox.ScreenController
 import org.mozilla.tv.firefox.ScreenControllerStateMachine.ActiveScreen.WEB_RENDER
 import org.mozilla.tv.firefox.ext.LiveDataCombiners
+import org.mozilla.tv.firefox.ext.forceExhaustive
 import org.mozilla.tv.firefox.ext.isUriYouTubeTV
 import org.mozilla.tv.firefox.ext.use
 import org.mozilla.tv.firefox.framework.FrameworkRepo
@@ -29,8 +34,10 @@ class CursorViewModel(
     screenController: ScreenController,
     sessionRepo: SessionRepo
 ) : ViewModel() {
+
     private val _touchSimulationLiveData = MutableLiveData<Consumable<MotionEvent>>()
     val touchSimulationLiveData: LiveData<Consumable<MotionEvent>> = _touchSimulationLiveData
+    private var prevDownMotionEvent: MotionEvent? = null
 
     private val isConfigurationWithOwnNavControls: LiveData<Boolean> = LiveDataCombiners.combineLatest(
         frameworkRepo.isVoiceViewEnabled,
@@ -57,9 +64,61 @@ class CursorViewModel(
      * manipulation work to [CursorViewModel]
      */
     fun onSelectKeyEvent(action: Int, pos: PointF) {
-        val now = SystemClock.uptimeMillis()
-        MotionEvent.obtain(now - DOWN_TIME_OFFSET_MILLIS, now, action, pos.x, pos.y, 0).use {
+        validateMotionEvent(action, SystemClock.uptimeMillis(), pos)?.use {
             _touchSimulationLiveData.value = Consumable.from(it)
         }
+    }
+
+    /**
+     * Problem: there is a distinctive time difference between [MotionEvent.ACTION_DOWN] and
+     * [MotionEvent.ACTION_UP] when using DPAD_SELECT_KEY. [GeckoSession] requires them to have matching
+     * [MotionEvent.getEventTime] in order to register such [KeyEvent] as "Click" touch event.
+     *
+     * [validateMotionEvent] checks for previous ACTION_DOWN vs. upcoming ACTION_UP to calculate
+     * the diff against [ViewConfiguration.getLongPressTimeout] to determine whether "Click" or
+     * "Long Press" event. Also filters unnecessary [MotionEvent] that occurs when holding down
+     * DPAD_SELECT_KEY (see (*)) by bypassing consecutive ACTION_DOWN calls.
+     *
+     * (*) Capture the first instance of [KeyEvent.ACTION_DOWN] upon long press and check against
+     * the first instance of [KeyEvent.ACTION_UP].
+     */
+    @VisibleForTesting
+    private fun validateMotionEvent(action: Int, now: Long, pos: PointF) : MotionEvent? {
+        var curr = MotionEvent.obtain(now - DOWN_TIME_OFFSET_MILLIS, now, action, pos.x, pos.y, 0)
+
+        // ACTION_DOWN == 0, ACTION_UP == 1
+        Log.d("ValidatedMotionEvent", String.format("curr: %d, prev: %d", action, prevDownMotionEvent?.action))
+
+        when (action) {
+            KeyEvent.ACTION_DOWN -> {
+                if (prevDownMotionEvent == null) {
+                    prevDownMotionEvent = MotionEvent.obtain(curr)
+                } else {
+                    return null
+                }
+            }
+            KeyEvent.ACTION_UP -> {
+                if (prevDownMotionEvent != null) {
+                    // Record the time diff between curr and prev event event times (>500 => LONG_PRESS)
+                    Log.d("updatedMotionEvent", String.format("%d", curr.eventTime - prevDownMotionEvent!!.eventTime))
+                    if (curr.eventTime - prevDownMotionEvent!!.eventTime < ViewConfiguration.getLongPressTimeout()) {
+                        // Register it as a "Click" Event and match the down & event time with the previous event
+                        curr = MotionEvent.obtain(prevDownMotionEvent!!.downTime, prevDownMotionEvent!!.eventTime, action, pos.x, pos.y, 0)
+                    }
+
+                    prevDownMotionEvent!!.recycle()
+                    prevDownMotionEvent = null
+                } else {
+                    return null
+                }
+            }
+            else -> {
+                // Fallback Option - See [MotionEvent#Consistency Guarantees]
+                prevDownMotionEvent!!.recycle()
+                prevDownMotionEvent = null
+            }
+        }.forceExhaustive
+
+        return curr
     }
 }
