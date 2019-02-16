e10f48ba36b20294fa99563758ca359b1e484aa1
Severin Rudie <srudie@mozilla.com>
2019-02-15 17:02:21 -0800

    Closes #1850: clarified YouTubeBackHandler code

--- app/src/main/java/org/mozilla/tv/firefox/webrender/YouTubeBackHandler.kt
+++ app/src/main/java/org/mozilla/tv/firefox/webrender/YouTubeBackHandler.kt
@@ -11,19 +11,24 @@ import org.mozilla.tv.firefox.MainActivity
 import org.mozilla.tv.firefox.ext.evalJS
 import org.mozilla.tv.firefox.ext.handleYoutubeBack
 
+// This will only happen if YouTube is loading or navigation has broken
+private const val noElementFocused = "document.activeElement === null"
+// This will only happen if YouTube is loading or navigation has broken
+private const val bodyElementFocused = "document.activeElement.tagName === \"BODY\""
+private const val sidebarFocused = "document.activeElement.parentElement.parentElement.id === 'guide-list'"
+
 /**
  * youtube/tv does not handle their back stack correctly. Going back in history visits redirects
  * which do not alter the UI. Users must navigate back by pressing ESC. To deal with this,
- * we remap BACK presses to ESC. When the top of YouTube's back stack is reached, we back
- * all the way out of YouTube.
+ * we remap BACK presses to ESC. When the top of YouTube's back stack is reached, or we are in
+ * an unexpected state, we back all the way out of YouTube.
  *
- * If YouTube guide-list element is focused:
+ * If YouTube guide-list element is focused or we are in an unexpected state:
  * - Suppress the DOWN,BACK key event
  * - Change the UP,BACK key event to go back in history before YouTube
  * Else:
  * - Dispatch ESC key event
  */
-
 class YouTubeBackHandler(
     private val event: KeyEvent,
     private val engineView: EngineView?,
@@ -31,25 +36,27 @@ class YouTubeBackHandler(
 ) {
 
     fun handleBackClick() {
-        val jsCallback = ValueCallback<String> {
-            if (it == "true") {
+
+        val shouldWeExitPage = """
+               (function () {
+                    return $noElementFocused ||
+                        $bodyElementFocused ||
+                        $sidebarFocused;
+                })();
+        """.trimIndent()
+
+        val backOrMoveFocus = ValueCallback<String> { shouldExitPage ->
+            if (shouldExitPage == "true") {
                 if (event.action == KeyEvent.ACTION_DOWN) Unit
                 else goBackBeforeYouTube()
             } else {
+                // Esc will move focus within YouTube
                 val escKeyEvent = KeyEvent(event.action, KeyEvent.KEYCODE_ESCAPE)
                 activity.dispatchKeyEvent(escKeyEvent)
             }
         }
-        // This will return true if there is no focused element, the body tag is focused, or the
-        // currently focused element is a button in the left sidebar. Otherwise it will return false
-        engineView?.evalJS("""
-                (function () {
-                    return document.activeElement === null ||
-                        document.activeElement.tagName === "BODY" ||
-                        document.activeElement.parentElement.parentElement.id === 'guide-list';
-                })();
-                """,
-                jsCallback)
+
+        engineView?.evalJS(javascript = shouldWeExitPage, callback = backOrMoveFocus)
     }
 
     private fun goBackBeforeYouTube() {
