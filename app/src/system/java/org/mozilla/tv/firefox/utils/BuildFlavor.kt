25ad12c4122e22c0ffa9b5fa85dc8adbfc7444b6
Simon Chae <chaesmn@gmail.com>
2019-03-15 11:58:44 -0400

    Issue #1919: Use EngineSettings.userAgentString + fix test

--- app/src/system/java/org/mozilla/tv/firefox/utils/BuildFlavor.kt
+++ app/src/system/java/org/mozilla/tv/firefox/utils/BuildFlavor.kt
@@ -6,9 +6,9 @@
 package org.mozilla.tv.firefox.utils
 
 import android.content.Context
-import android.webkit.WebSettings
 import mozilla.components.concept.engine.request.RequestInterceptor
 import org.mozilla.tv.firefox.BuildConfig
+import org.mozilla.tv.firefox.ext.webRenderComponents
 
 /**
  * Provides build flavor specific tasks
@@ -18,20 +18,22 @@ class BuildFlavor {
         if (isDevBuild) {
             return "DEBUG / " +
                     "FLAVOR: ${BuildConfig.FLAVOR} / " +
-                    "VERSION: ${BuildConfig.VERSION_NAME}" // TODO: show WebView version?
+                    "VERSION: ${BuildConfig.VERSION_NAME}"
         }
 
         return null
     }
 
     fun getEngineVersion(context: Context): String {
-        val userAgent = WebSettings.getDefaultUserAgent(context)
+        val userAgent = context.webRenderComponents.engine.settings.userAgentString
         val regex = "Chrome[^\\s]+".toRegex()
         val chromePrefixIndex = 7
         var chromeVersion = ""
 
-        regex.find(userAgent)?.let {
-            chromeVersion = it.value.substring(chromePrefixIndex)
+        userAgent?.apply {
+            regex.find(this)?.let {
+                chromeVersion = it.value.substring(chromePrefixIndex)
+            }
         }
 
         return chromeVersion
