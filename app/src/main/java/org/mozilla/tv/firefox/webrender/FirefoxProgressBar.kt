9b5175aec3fb694d9909608cddacbbb10cfcb5e8
Severin Rudie <srudie@mozilla.com>
2019-03-19 20:40:30 -0700

    Issue #1907: removes progress bar hide delay
    Per UX https://github.com/mozilla-mobile/firefox-tv/issues/1907#issuecomment-474663069

--- app/src/main/java/org/mozilla/tv/firefox/webrender/FirefoxProgressBar.kt
+++ app/src/main/java/org/mozilla/tv/firefox/webrender/FirefoxProgressBar.kt
@@ -6,31 +6,22 @@ package org.mozilla.tv.firefox.webrender
 
 import android.content.Context
 import android.graphics.drawable.AnimationDrawable
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.firefox_progress_bar.view.*
+import kotlinx.android.synthetic.main.firefox_progress_bar.view.progressAnimation
+import kotlinx.android.synthetic.main.firefox_progress_bar.view.url
 import mozilla.components.browser.session.Session
 import org.mozilla.tv.firefox.R
-import java.lang.ref.WeakReference
-import java.util.concurrent.TimeUnit
 
-private const val HIDE_MESSAGE_ID = 0
 private const val HIDE_ANIMATION_DURATION_MILLIS = 250L
-private val HIDE_AFTER_MILLIS = TimeUnit.SECONDS.toMillis(1)
 
 class FirefoxProgressBar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyle: Int = 0
 ) : LinearLayout(context, attrs, defStyle), Session.Observer {
-
-    private val hideHandler = FirefoxProgressBarHideHandler(this)
 
     fun initialize(webRenderFrag: WebRenderFragment) {
         webRenderFrag.session.register(this, webRenderFrag)
@@ -40,7 +31,7 @@ class FirefoxProgressBar @JvmOverloads constructor(
         if (loading) {
             showBar()
         } else {
-            scheduleHideBar()
+            hideBar()
         }
     }
 
@@ -55,40 +46,20 @@ class FirefoxProgressBar @JvmOverloads constructor(
     }
 
     private fun showBar() {
-        hideHandler.removeMessages(HIDE_MESSAGE_ID)
         visibility = View.VISIBLE
         (progressAnimation.background as AnimationDrawable).start()
         animate().cancel()
         alpha = 1f
     }
 
-    private fun scheduleHideBar() {
-        hideHandler.removeMessages(HIDE_MESSAGE_ID)
-        hideHandler.sendEmptyMessageDelayed(HIDE_MESSAGE_ID, HIDE_AFTER_MILLIS)
-    }
-}
-
-/**
- * Hides the cursor when it receives a message.
- *
- * We use a [Handler], with [Message]s, because they make no allocations, unlike
- * more modern/readable approaches:
- * - coroutines
- * - Animators with start delays (and cancelling them as necessary)
- */
-private class FirefoxProgressBarHideHandler(view: FirefoxProgressBar) : Handler(Looper.getMainLooper()) {
-    private val viewWeakReference = WeakReference<FirefoxProgressBar>(view)
-
-    override fun handleMessage(msg: Message?) {
-        val progressBar = viewWeakReference.get()
-        progressBar
-                ?.animate()
-                ?.withEndAction {
-                    progressBar.visibility = GONE
-                    (progressBar.progressAnimation.background as AnimationDrawable).stop()
+    private fun hideBar() {
+        this.animate()
+                .withEndAction {
+                    this.visibility = View.GONE
+                    (this.progressAnimation.background as AnimationDrawable).stop()
                 }
-                ?.setDuration(HIDE_ANIMATION_DURATION_MILLIS)
-                ?.alpha(0f)
-                ?.start()
+                .setDuration(HIDE_ANIMATION_DURATION_MILLIS)
+                .alpha(0f)
+                .start()
     }
 }
