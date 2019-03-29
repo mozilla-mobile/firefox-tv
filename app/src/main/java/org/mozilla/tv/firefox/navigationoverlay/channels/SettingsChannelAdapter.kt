5b1041151c5db5e8951e952eb22626f4b2758017
Chenxia Liu <liuche@mozilla.com>
2019-03-29 13:49:44 -0700

    Issue #689 - Launch Settings fragment

--- app/src/main/java/org/mozilla/tv/firefox/navigationoverlay/channels/SettingsChannelAdapter.kt
+++ app/src/main/java/org/mozilla/tv/firefox/navigationoverlay/channels/SettingsChannelAdapter.kt
@@ -13,7 +13,8 @@ import org.mozilla.tv.firefox.R
 import org.mozilla.tv.firefox.utils.URLs
 
 class SettingsChannelAdapter(
-        private val loadUrl: (String) -> Unit
+        private val loadUrl: (String) -> Unit,
+        private val showSettings: (SettingsType) -> Unit
 ) : RecyclerView.Adapter<SettingsTileHolder>() {
     private val settingsItems = arrayOf(
             SettingsItem(
@@ -46,9 +47,9 @@ class SettingsChannelAdapter(
         iconView.setImageResource(settingsItems[position].imgRes)
         titleView.setText(settingsItems[position].titleRes)
         itemView.setOnClickListener {
-            when (settingsItems.get(position).type) {
-                SettingsType.DATA_COLLECTION -> return@setOnClickListener // TODO
-                SettingsType.CLEAR_COOKIES -> return@setOnClickListener // TODO
+            when (val type = settingsItems[position].type) {
+                SettingsType.DATA_COLLECTION -> showSettings(type)
+                SettingsType.CLEAR_COOKIES -> showSettings(type)
                 SettingsType.ABOUT -> loadUrl(URLs.URL_ABOUT)
                 SettingsType.PRIVACY_POLICY -> loadUrl(URLs.PRIVACY_NOTICE_URL)
             }
