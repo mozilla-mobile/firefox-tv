29d6f77df931df91cb02325011a77a4be4058eb4
Simon Chae <chaesmn@gmail.com>
2019-04-02 17:21:44 -0400

    Closes #1933: Update GeckoEngineSession()
    to reflect Session arg deprecation

--- app/src/gecko/java/org/mozilla/tv/firefox/ext/EngineSession.kt
+++ app/src/gecko/java/org/mozilla/tv/firefox/ext/EngineSession.kt
@@ -5,8 +5,7 @@
 package org.mozilla.tv.firefox.ext
 
 import android.content.Context
-import mozilla.components.browser.session.Session
 import mozilla.components.concept.engine.EngineSession
 
 // This is only required for [SystemEngineSession]
-fun EngineSession.resetView(@Suppress("UNUSED_PARAMETER") context: Context, @Suppress("UNUSED_PARAMETER") session: Session? = null) { }
+fun EngineSession.resetView(@Suppress("UNUSED_PARAMETER") context: Context) { }
