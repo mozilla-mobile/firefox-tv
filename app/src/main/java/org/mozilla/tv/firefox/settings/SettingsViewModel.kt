8a4823f003d61bec8a5e8fa56a7832f10fe8a116
Michael Comella <michael.l.comella@gmail.com>
2019-04-17 11:42:09 -0700

    Issue #1912: Remove unused variable from SettingsViewModel.

--- app/src/main/java/org/mozilla/tv/firefox/settings/SettingsViewModel.kt
+++ app/src/main/java/org/mozilla/tv/firefox/settings/SettingsViewModel.kt
@@ -4,18 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import mozilla.components.support.base.observer.Consumable
-import org.mozilla.tv.firefox.framework.FrameworkRepo
 import org.mozilla.tv.firefox.session.SessionRepo
 import org.mozilla.tv.firefox.webrender.EngineViewCache
 
 class SettingsViewModel(
-    frameworkRepo: FrameworkRepo,
     private val settingsRepo: SettingsRepo,
     private val sessionRepo: SessionRepo
 ) : ViewModel() {
     private var _events = MutableLiveData<Consumable<SettingsFragment.Action>>()
-
-    val isVoiceViewEnabled = frameworkRepo.isVoiceViewEnabled
 
     val events: LiveData<Consumable<SettingsFragment.Action>> = _events
     val dataCollectionEnabled = settingsRepo.dataCollectionEnabled
