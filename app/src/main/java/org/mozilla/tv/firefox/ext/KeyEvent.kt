d4c5c9bb5f2443b5a78422380ad814d67607d138
Severin Rudie <srudie@mozilla.com>
2019-04-23 16:10:50 -0700

    Issue #1912: Address some review nits.

--- app/src/main/java/org/mozilla/tv/firefox/ext/KeyEvent.kt
+++ app/src/main/java/org/mozilla/tv/firefox/ext/KeyEvent.kt
@@ -8,7 +8,18 @@ import android.graphics.PointF
 import android.view.KeyEvent
 import android.view.MotionEvent
 import androidx.annotation.CheckResult
+import org.mozilla.tv.firefox.utils.Direction
 
 @CheckResult(suggest = "Recycle the MotionEvent after use")
 fun KeyEvent.toMotionEvent(pos: PointF): MotionEvent =
     MotionEvent.obtain(this.downTime, this.eventTime, this.action, pos.x, pos.y, 0)
+
+fun KeyEvent.toDirection(): Direction? {
+    return when (this.keyCode) {
+        KeyEvent.KEYCODE_DPAD_UP -> Direction.UP
+        KeyEvent.KEYCODE_DPAD_DOWN -> Direction.DOWN
+        KeyEvent.KEYCODE_DPAD_LEFT -> Direction.LEFT
+        KeyEvent.KEYCODE_DPAD_RIGHT -> Direction.RIGHT
+        else -> null
+    }
+}
