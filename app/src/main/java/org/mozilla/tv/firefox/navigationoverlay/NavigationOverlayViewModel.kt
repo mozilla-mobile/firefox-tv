daf309e9b8db54907cbad3770ed19140b5c425b0
Severin Rudie <srudie@mozilla.com>
2019-04-30 15:21:56 -0700

    Issue #2110: add pinned tiles to OverlayVM

--- app/src/main/java/org/mozilla/tv/firefox/navigationoverlay/NavigationOverlayViewModel.kt
+++ app/src/main/java/org/mozilla/tv/firefox/navigationoverlay/NavigationOverlayViewModel.kt
@@ -8,15 +8,20 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.Observable
 import org.mozilla.tv.firefox.ScreenControllerStateMachine.ActiveScreen
+import org.mozilla.tv.firefox.channel.ScreenshotStoreWrapper
 import org.mozilla.tv.firefox.ext.map
 import org.mozilla.tv.firefox.focus.FocusRepo
+import org.mozilla.tv.firefox.pinnedtile.PinnedTileRepo
 import org.mozilla.tv.firefox.session.SessionRepo
 import org.mozilla.tv.firefox.utils.URLs
 
 class NavigationOverlayViewModel(
-    sessionRepo: SessionRepo,
-    focusRepo: FocusRepo
+        sessionRepo: SessionRepo,
+        focusRepo: FocusRepo,
+        private val pinnedTileRepo: PinnedTileRepo,
+        screenshotStoreWrapper: ScreenshotStoreWrapper
 ) : ViewModel() {
+
     val focusUpdate = focusRepo.focusUpdate
     val focusRequests: Observable<Int> = focusRepo.defaultViewAfterScreenChange
             .filter { it.second == ActiveScreen.NAVIGATION_OVERLAY }
@@ -25,5 +30,12 @@ class NavigationOverlayViewModel(
     @Suppress("DEPRECATION")
     val viewIsSplit: LiveData<Boolean> = sessionRepo.legacyState.map {
         it.currentUrl != URLs.APP_URL_HOME
+    }
+
+    val pinnedTiles = pinnedTileRepo.pinnedTiles
+            .map { it.values.map { it.toChannelTile(screenshotStoreWrapper) } }
+
+    fun unpin(url: String) {
+        pinnedTileRepo.removePinnedTile(url)
     }
 }
