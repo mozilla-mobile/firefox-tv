d4061acb6f7fe35ef57e6863d9fee062f182a859
Michael Comella <michael.l.comella@gmail.com>
2019-05-13 13:29:31 -0700

    Issue #2240: Add KeyEvent.isKeyCodeSelect.

--- app/src/main/java/org/mozilla/tv/firefox/ext/KeyEvent.kt
+++ app/src/main/java/org/mozilla/tv/firefox/ext/KeyEvent.kt
@@ -10,6 +10,14 @@ import android.view.MotionEvent
 import androidx.annotation.CheckResult
 import org.mozilla.tv.firefox.utils.Direction
 
+/**
+ * Returns true if the [KeyEvent] represents a SELECT event. The SELECT button on device (DPAD_CENTER)
+ * and on the emulator (ENTER) are different so it's preferred to use this method over checking the
+ * keys individually.
+ */
+val KeyEvent.isKeyCodeSelect: Boolean
+    get() = keyCode == KeyEvent.KEYCODE_DPAD_CENTER || keyCode == KeyEvent.KEYCODE_ENTER
+
 @CheckResult(suggest = "Recycle the MotionEvent after use")
 fun KeyEvent.toMotionEvent(pos: PointF): MotionEvent =
     MotionEvent.obtain(this.downTime, this.eventTime, this.action, pos.x, pos.y, 0)
