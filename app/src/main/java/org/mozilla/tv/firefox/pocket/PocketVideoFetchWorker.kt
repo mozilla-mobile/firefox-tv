528a7c5507a7baaa192f814532f47f473067ccb7
Michael Comella <michael.l.comella@gmail.com>
2019-05-24 17:03:21 -0700

    Issue #2325: Add tests for PocketVideoFetchWorker; improve docs.

--- app/src/main/java/org/mozilla/tv/firefox/pocket/PocketVideoFetchWorker.kt
+++ app/src/main/java/org/mozilla/tv/firefox/pocket/PocketVideoFetchWorker.kt
@@ -12,6 +12,10 @@ import org.mozilla.tv.firefox.ext.serviceLocator
 
 /**
  * A background task that fetches the Pocket video recommendations and stores them to disk, if valid.
+ *
+ * IMPORTANT: to avoid overloading the server, job requests using this worker are expected to:
+ * - Avoid scheduling jobs for many clients at the same time (e.g. all clients at refresh at 3am)
+ * - Add randomness to their setBackoffCriteria timing (for errors)
  */
 class PocketVideoFetchWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams) {
 
@@ -24,7 +28,9 @@ class PocketVideoFetchWorker(appContext: Context, workerParams: WorkerParameters
     override fun doWork(): Result {
         val rawJSONStr = runBlocking { pocketEndpointRaw.getGlobalVideoRecommendations() }
         if (rawJSONStr == null) {
-            // If the connection to the server failed, we should try to reconnect.
+            // If the connection to the server failed, we should try to reconnect. Beyond typical
+            // scheduling delays, WorkManager does not add randomness to this retry logic: as
+            // mentioned in the class kdoc, job requests are expected to add their own randomness.
             return Result.retry()
         }
 
