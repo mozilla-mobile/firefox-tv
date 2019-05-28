e47730a91a3e1fbe70bf9c2da40c205a4f451fbf
Severin Rudie <srudie@mozilla.com>
2019-05-28 11:20:38 -0700

    Issue #2195: added news channel content

---
+++ app/src/main/java/org/mozilla/tv/firefox/channels/content/ChannelContent.kt
@@ -0,0 +1,25 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.channels.content
+
+import android.widget.ImageView
+
+/**
+ * Namespace for retrieval of data objects that back 'TV Guide'-style channel
+ * content.
+ *
+ * Functions are added as extension functions so that they can be organized
+ * into separate files.
+ */
+object ChannelContent {
+
+    /**
+     * Helper function shared by various content files. See [getNewsChannels]
+     * for an example.
+     */
+    fun setImage(id: Int): (ImageView) -> Unit {
+        return { imageView -> imageView.setImageResource(id) }
+    }
+}
---
+++ app/src/main/java/org/mozilla/tv/firefox/channels/content/NewsChannels.kt
@@ -0,0 +1,84 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.channels.content
+
+import org.mozilla.tv.firefox.R
+import org.mozilla.tv.firefox.channels.ChannelTile
+import org.mozilla.tv.firefox.channels.TileSource
+
+fun ChannelContent.getNewsChannels(): List<ChannelTile> = listOf(
+    ChannelTile(
+        url = "https://us.cnn.com/videos",
+        title = "CNN",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "cnn"
+    ),
+    ChannelTile(
+        url = "https://video.foxnews.com/",
+        title = "Fox News",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "foxNews"
+    ),
+    ChannelTile(
+        url = "https://www.washingtonpost.com/video/",
+        title = "Washington Post",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "washingtonPost"
+    ),
+    ChannelTile(
+        url = "https://www.usatoday.com/media/latest/videos/news/",
+        title = "USA Today",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "usaToday"
+    ),
+    ChannelTile(
+        url = "https://www.wsj.com/video/",
+        title = "Wall Street Journal",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "wallStreetJournal"
+    ),
+    ChannelTile(
+        url = "https://www.nbcnews.com/video",
+        title = "NBC News",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "nbcNews"
+    ),
+    ChannelTile(
+        url = "https://www.cbsnews.com/video/",
+        title = "CBS News",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "cbsNews"
+    ),
+    ChannelTile(
+        url = "https://www.huffpost.com/section/video",
+        title = "Huffington Post",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "huffingtonPost"
+    ),
+    ChannelTile(
+        url = "https://video.vice.com/",
+        title = "Vice",
+        subtitle = null,
+        setImage = setImage(R.drawable.cursor_active_bg), // TODO replace with actual image
+        tileSource = TileSource.TV_GUIDE,
+        id = "vice"
+    )
+)
