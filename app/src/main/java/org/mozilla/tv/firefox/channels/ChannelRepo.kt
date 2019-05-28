8c9a316206e5a88d138112238da2e8038c3e12ec
Severin Rudie <srudie@mozilla.com>
2019-05-28 11:30:06 -0700

    Issue #2195: added news tile retrieval to repo

--- app/src/main/java/org/mozilla/tv/firefox/channels/ChannelRepo.kt
+++ app/src/main/java/org/mozilla/tv/firefox/channels/ChannelRepo.kt
@@ -4,6 +4,11 @@
 
 package org.mozilla.tv.firefox.channels
 
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.Observables
+import io.reactivex.subjects.BehaviorSubject
+import org.mozilla.tv.firefox.channels.content.ChannelContent
+import org.mozilla.tv.firefox.channels.content.getNewsChannels
 import org.mozilla.tv.firefox.channels.pinnedtile.PinnedTileRepo
 import org.mozilla.tv.firefox.telemetry.TelemetryIntegration
 
@@ -16,6 +21,10 @@ import org.mozilla.tv.firefox.telemetry.TelemetryIntegration
 class ChannelRepo(
     private val pinnedTileRepo: PinnedTileRepo
 ) {
+
+    fun getNewsTiles(): Observable<List<ChannelTile>> =
+        bundledNewsTiles.filterNotBlacklisted(blacklistedNewsIds)
+
     fun removeChannelContent(tileData: ChannelTile) {
         when (tileData.tileSource) {
             TileSource.BUNDLED, TileSource.CUSTOM -> {
@@ -26,4 +35,18 @@ class ChannelRepo(
             TileSource.TV_GUIDE -> Unit //TODO in #2326
         }
     }
+
+    private val bundledNewsTiles = Observable.just(ChannelContent.getNewsChannels())
+        .replay(1)
+        .autoConnect(0)
+    //TODO in #2326 (replace emptyList with blacklist. Push any updates to this subject)
+    private val blacklistedNewsIds = BehaviorSubject.createDefault(emptyList<String>())
+
+}
+
+private fun Observable<List<ChannelTile>>.filterNotBlacklisted(
+    blacklistIds: Observable<List<String>>
+): Observable<List<ChannelTile>> {
+    return Observables.combineLatest(this, blacklistIds)
+        .map { (tiles, blacklistIds) -> tiles.filter { !blacklistIds.contains(it.id) } }
 }
