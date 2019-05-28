c0a714265d5988cbfb2b894236f92d4e29268025
Severin Rudie <srudie@mozilla.com>
2019-05-28 13:42:59 -0700

    Issue #2195: added data for sports and music channels

---
+++ app/src/main/java/org/mozilla/tv/firefox/channels/content/MusicChannels.kt
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.channels.content
+
+import org.mozilla.tv.firefox.R
+import org.mozilla.tv.firefox.channels.ChannelTile
+import org.mozilla.tv.firefox.channels.TileSource
+
+fun ChannelContent.getMusicChannels(): List<ChannelTile> = listOf(
+    ChannelTile(
+        url = "https://www.npr.org/stations/",
+        title = "NPR Stations", // TODO different name?
+        subtitle = null,
+        setImage = setImage(R.drawable.tile_music_npr),
+        tileSource = TileSource.TV_GUIDE,
+        id = "nprStations"
+    ),
+    ChannelTile(
+        url = "https://bandcamp.com/#discover",
+        title = "Bandcamp",
+        subtitle = null,
+        setImage = setImage(R.drawable.tile_music_bandcamp),
+        tileSource = TileSource.TV_GUIDE,
+        id = "bandcamp"
+    )
+)
