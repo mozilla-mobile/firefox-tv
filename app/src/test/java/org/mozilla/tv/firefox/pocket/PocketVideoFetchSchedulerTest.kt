df6ce3858cbf1b46698a20ffe056ee9cc14f5f62
Michael Comella <michael.l.comella@gmail.com>
2019-05-31 16:48:12 -0700

    Issue #2325: Add test seam and tests for PocketVideoFetchScheduler.

--- app/src/test/java/org/mozilla/tv/firefox/pocket/PocketVideoFetchSchedulerTest.kt
+++ app/src/test/java/org/mozilla/tv/firefox/pocket/PocketVideoFetchSchedulerTest.kt
@@ -12,16 +12,21 @@ import androidx.work.WorkManager
 import io.mockk.MockKAnnotations
 import io.mockk.called
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import io.mockk.slot
+import io.mockk.spyk
 import io.mockk.verify
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mozilla.tv.firefox.pocket.PocketVideoFetchScheduler.Companion.FETCH_END_HOUR
+import org.mozilla.tv.firefox.pocket.PocketVideoFetchScheduler.Companion.FETCH_START_HOUR
+import org.robolectric.RobolectricTestRunner
+import java.util.Calendar
 
+@RunWith(RobolectricTestRunner::class)
 class PocketVideoFetchSchedulerTest {
 
     private lateinit var scheduler: PocketVideoFetchScheduler
@@ -38,7 +43,7 @@ class PocketVideoFetchSchedulerTest {
 
     @Test
     fun `GIVEN fetch interval constants THEN start hour is less than end hour`() {
-        assertTrue(PocketVideoFetchScheduler.FETCH_START_HOUR < PocketVideoFetchScheduler.FETCH_END_HOUR)
+        assertTrue(FETCH_START_HOUR < PocketVideoFetchScheduler.FETCH_END_HOUR)
     }
 
     @Test
@@ -46,14 +51,13 @@ class PocketVideoFetchSchedulerTest {
         assertTrue(PocketVideoFetchScheduler.BACKOFF_DELAY_MIN_MILLIS < PocketVideoFetchScheduler.BACKOFF_DELAY_MAX_MILLIS)
     }
 
-    @Ignore // TODO: the WorkManager dependency breaks calling onStart; after we commit the other tests, fix this test.
     @Test
     fun `WHEN onStart is called THEN schedulePocketBackgroundFetch is called`() {
         // This test is tightly coupled to the implementation but, given this daily background job will rarely be
         // tested, we want to verify the implementation hasn't accidentally changed in a way that broke the functionality.
-        val schedulerMock = mockk<PocketVideoFetchScheduler>(relaxUnitFun = true)
+        val schedulerMock = spyk(PocketVideoFetchScheduler { true })
         schedulerMock.onStart()
-        verify(exactly = 1) { schedulerMock.schedulePocketBackgroundFetch(any()) }
+        verify(exactly = 1) { schedulerMock.schedulePocketBackgroundFetch(any(), any(), any()) }
     }
 
     @Test
@@ -101,8 +105,47 @@ class PocketVideoFetchSchedulerTest {
         assertNotEquals(fromArg, untilArg) // Sanity check to verify test correctness on the tricky capturing above.
     }
 
-    // TODO: test initial delay in work request
-    // TODO: test getDelayUntilUpcomingNightFetchMillis logic (should we test it directly or as part of the request?)
+    @Test
+    fun `GIVEN there is no randomness WHEN scheduling a pocket background fetch THEN the initial delay is the millis until the expected fetch time`() {
+        val now = Calendar.getInstance().apply {
+            set(1, 1, 1, 2, 30, 30)
+            set(Calendar.MILLISECOND, 500)
+        }
+        val expectedFetchTimeForNoRandomness = Calendar.getInstance().apply {
+            set(1, 1, 2, FETCH_START_HOUR, 0, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        // We'd return zero but randLong is used somewhere else and then we'd need robolectric.
+        // Implicit assumption that 0 is minimum for this.
+        val randLong = { from: Long, _: Long -> from }
+        val expected = expectedFetchTimeForNoRandomness.timeInMillis - now.timeInMillis
+
+        scheduler.schedulePocketBackgroundFetch(workManager, now, randLong)
+
+        val request = verifyWorkManagerEnqueueAndCaptureRequest()
+        assertEquals(expected, request.workSpec.initialDelay)
+    }
+
+    @Test
+    fun `GIVEN there is randomness WHEN scheduling a pocket background fetch THEN the initial delay is the millis until the expected fetch time plus random millis`() {
+        val now = Calendar.getInstance().apply {
+            set(1, 1, 1, 2, 30, 30)
+            set(Calendar.MILLISECOND, 500)
+        }
+        val nextFetchIntervalEndTime = Calendar.getInstance().apply {
+            set(1, 1, 2, FETCH_END_HOUR.toInt(), 0, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        val randLong = { _: Long, until: Long -> until }
+        val expected = nextFetchIntervalEndTime.timeInMillis - now.timeInMillis
+
+        scheduler.schedulePocketBackgroundFetch(workManager, now, randLong)
+
+        val request = verifyWorkManagerEnqueueAndCaptureRequest()
+        assertEquals(expected, request.workSpec.initialDelay)
+    }
 
     private fun verifyWorkManagerEnqueueAndCaptureRequest(): OneTimeWorkRequest = slot<OneTimeWorkRequest>().also {
         verify { workManager.enqueueUniqueWork(any(), any(), capture(it)) }
