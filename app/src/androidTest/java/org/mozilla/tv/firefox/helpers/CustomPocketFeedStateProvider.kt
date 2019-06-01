5af8b1ce2751c63bf3181af6c8e02aab412363ca
Michael Comella <michael.l.comella@gmail.com>
2019-06-01 10:24:11 -0700

    Issue #2325 - post: Correct initialization error in YouTubeNavigationTest.

--- app/src/androidTest/java/org/mozilla/tv/firefox/helpers/CustomPocketFeedStateProvider.kt
+++ app/src/androidTest/java/org/mozilla/tv/firefox/helpers/CustomPocketFeedStateProvider.kt
@@ -2,23 +2,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+@file:Suppress("DEPRECATION") // PocketVideoParser
+
 package org.mozilla.tv.firefox.helpers
 
 import android.app.Application
 import io.reactivex.subjects.BehaviorSubject
-import org.mozilla.tv.firefox.ext.serviceLocator
+import org.mozilla.tv.firefox.pocket.PocketVideoJSONValidator
+import org.mozilla.tv.firefox.pocket.PocketVideoParser
 import org.mozilla.tv.firefox.pocket.PocketVideoRepo
+import org.mozilla.tv.firefox.pocket.PocketVideoStore
 
 /**
  * Provides a fake [PocketVideoRepo] implementation for testing purposes.
  *
  * Any values pushed to [fakedPocketRepoState] will be immediately emitted.
  */
-class CustomPocketFeedStateProvider(private val appContext: Application) {
+class CustomPocketFeedStateProvider(appContext: Application) {
 
     val fakedPocketRepoState = BehaviorSubject.create<PocketVideoRepo.FeedState>()
     val fakedPocketRepo = PocketVideoRepo(
-        appContext.serviceLocator.pocketVideoStore,
+        PocketVideoStore(
+            appContext,
+            appContext.assets,
+            PocketVideoJSONValidator(PocketVideoParser)
+        ),
         isPocketEnabledByLocale = { true },
         _feedState = fakedPocketRepoState
     )
