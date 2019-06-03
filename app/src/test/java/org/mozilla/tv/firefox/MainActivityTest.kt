71e6969e7c7294efa849cf7e1b52f03465f5007d
Michael Comella <michael.l.comella@gmail.com>
2019-06-03 16:43:52 -0700

    Closes #2367: Add sanity test for MainActivity start up.

---
+++ app/src/test/java/org/mozilla/tv/firefox/MainActivityTest.kt
@@ -0,0 +1,26 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class MainActivityTest {
+
+    // sanity check:
+    // 1) we need at least one start up test to ensure we don't regress Robolectric.buildActivity working correctly.
+    // 2) devs run unit tests before pushing but generally don't run UI tests so we can tighten their feedback loop
+    // here, in unit tests, by alerting them if MainActivity crashes on regular startup.
+    @Test
+    fun `WHEN MainActivity starts up regularly THEN it does not crash`() {
+        Robolectric.buildActivity(MainActivity::class.java)
+            .create()
+            .start()
+            .resume()
+    }
+}
