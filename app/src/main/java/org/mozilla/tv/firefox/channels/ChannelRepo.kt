4d120f725547ea994e36d2fed719c9d5724eeb93
Michael Comella <michael.l.comella@gmail.com>
2019-06-11 13:58:11 -0400

    Closes #2406: ChannelRepo channels return data raw from data source.

--- app/src/main/java/org/mozilla/tv/firefox/channels/ChannelRepo.kt
+++ app/src/main/java/org/mozilla/tv/firefox/channels/ChannelRepo.kt
@@ -27,15 +27,12 @@ class ChannelRepo(
 
     fun getNewsTiles(): Observable<List<ChannelTile>> =
         bundledNewsTiles.filterNotBlacklisted(blacklistedNewsIds)
-            .map { it.padToTen() }
 
     fun getSportsTiles(): Observable<List<ChannelTile>> =
         bundledSportsTiles.filterNotBlacklisted(blacklistedSportsIds)
-            .map { it.padToTen() }
 
     fun getMusicTiles(): Observable<List<ChannelTile>> =
         bundledMusicTiles.filterNotBlacklisted(blacklistedMusicIds)
-            .map { it.padToTen() }
 
     fun removeChannelContent(tileData: ChannelTile) {
         when (tileData.tileSource) {
@@ -73,17 +70,4 @@ fun Observable<List<ChannelTile>>.filterNotBlacklisted(
 ): Observable<List<ChannelTile>> {
     return Observables.combineLatest(this, blacklistIds)
         .map { (tiles, blacklistIds) -> tiles.filter { !blacklistIds.contains(it.id) } }
-}
-
-/**
- * Temporary method that will be removed before shipping. This allows us to test the UX
- * for channels that do not yet have 10 approved sites
- *
- * See #2195 ("For categories that don't have 10 tiles, add duplicates of approved
- * channels so we have an idea of what they look like.")
- */
-private fun List<ChannelTile>.padToTen(): List<ChannelTile> {
-    return (0 until 10)
-        .flatMap { this }
-        .take(10)
 }
