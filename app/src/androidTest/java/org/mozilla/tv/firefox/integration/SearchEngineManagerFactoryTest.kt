cb86570d37fbab669cd1c1fad570bd1840905d60
Severin Rudie <srudie@mozilla.com>
2019-07-09 15:04:37 -0700

    Issue #2462: adds a regression test for missing search codes

---
+++ app/src/androidTest/java/org/mozilla/tv/firefox/integration/SearchEngineManagerFactoryTest.kt
@@ -0,0 +1,34 @@
+package org.mozilla.tv.firefox.integration
+
+import androidx.test.core.app.ApplicationProvider
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import org.mozilla.tv.firefox.FirefoxApplication
+import org.mozilla.tv.firefox.search.SearchEngineManagerFactory
+import org.mozilla.tv.firefox.search.SearchEngineProviderWrapper
+
+/**
+ * This is an integration test between [SearchEngineManagerFactory], [SearchEngineProviderWrapper],
+ * and A-C that ensures our search codes are not lost due to a change in any of those three places.
+ */
+class SearchEngineManagerFactoryTest {
+
+    lateinit var app: FirefoxApplication
+
+    @Before
+    fun setup() {
+        app = ApplicationProvider.getApplicationContext()
+    }
+
+    @Test
+    fun searchEnginesShouldIncludeFftvSearchCodes() {
+        val searchEngineManager = SearchEngineManagerFactory.create(app)
+        val searchEngines = searchEngineManager.getSearchEngines(app)
+        val searchIds = searchEngines.map { it.identifier }
+
+        assertTrue(searchIds.contains(SearchEngineManagerFactory.AMAZON_SEARCH_CODE))
+        // We don't test for AMAZON_SEARCH_CODE_US_ONLY so that this test won't
+        // fail when run outside of the US
+    }
+}
