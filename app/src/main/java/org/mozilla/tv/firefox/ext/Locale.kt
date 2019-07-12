cef3286fe0d79bbe152a397dec0977b218d6e4f9
Michelle Wong <mwong@mozilla.com>
2019-07-12 16:14:20 -0700

    Issue #2543: added comment for Locale.languageAndMaybeCountryMatch

--- app/src/main/java/org/mozilla/tv/firefox/ext/Locale.kt
+++ app/src/main/java/org/mozilla/tv/firefox/ext/Locale.kt
@@ -2,7 +2,11 @@ package org.mozilla.tv.firefox.ext
 
 import java.util.*
 
-
+/**
+ * Returns true if the following is true for _any_ of the elements in [allowedLocales]:
+ * 1) Language == [this] language
+ * 2) No country selected || [this] country is selected country
+ */
 fun Locale.languageAndMaybeCountryMatch(allowedLocales: Array<out Locale>?): Boolean {
     allowedLocales ?: return false
     return allowedLocales.any { allowed ->
