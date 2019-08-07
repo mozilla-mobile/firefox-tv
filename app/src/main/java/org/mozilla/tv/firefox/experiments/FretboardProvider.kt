207b7ed6d25aef1ad5d80fda3441a5e791729233
Mathieu Leplatre <mathieu@mozilla.com>
2019-08-07 09:19:39 +0200

    Issue #2651: Fix Remote Settings URL

--- app/src/main/java/org/mozilla/tv/firefox/experiments/FretboardProvider.kt
+++ app/src/main/java/org/mozilla/tv/firefox/experiments/FretboardProvider.kt
@@ -19,7 +19,7 @@ import java.io.File
 import kotlin.coroutines.CoroutineContext
 
 const val EXPERIMENTS_JSON_FILENAME = "experiments.json"
-const val EXPERIMENTS_BASE_URL = "https://settings.prod.mozaws.net/v1"
+const val EXPERIMENTS_BASE_URL = "https://firefox.settings.services.mozilla.com/v1"
 const val EXPERIMENTS_BUCKET_NAME = "main"
 const val EXPERIMENTS_COLLECTION_NAME = "fftv-experiments"
 
