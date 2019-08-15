71db2d5182d81ef9a69e48b96cef49c7a8606819
Daniela Arcese <darcese@mozilla.com>
2019-08-15 17:24:55 -0400

    Issue #2528: Create custom robolectric test runner
    Needed to apply ADM class shadows to all robolectric classes

---
+++ app/src/test/java/org/mozilla/tv/firefox/helpers/FirefoxRobolectricTestRunner.kt
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.helpers
+
+import org.mozilla.tv.firefox.helpers.shadows.ShadowADMMessageHandlerBase
+import org.mozilla.tv.firefox.helpers.shadows.ShadowADMMessageReceiver
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+/**
+ * A Robolectric test runner that initializes the test environment for our unit tests.
+ */
+
+class FirefoxRobolectricTestRunner(testClass: Class<*>) : RobolectricTestRunner(testClass) {
+
+    override fun buildGlobalConfig(): Config {
+        val defaultConfig = super.buildGlobalConfig()
+
+        // See [ShadowADMMessageReceiver] and [ShadowADMMessageHandlerBase] for reason why these shadows are needed
+        val shadows = defaultConfig.shadows +
+            arrayOf(ShadowADMMessageHandlerBase::class, ShadowADMMessageReceiver::class)
+
+        return Config.Builder(defaultConfig)
+            .setShadows(shadows.map { it.java }.toTypedArray())
+            .build()
+    }
+}
