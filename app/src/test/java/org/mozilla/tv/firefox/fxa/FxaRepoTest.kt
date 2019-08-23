05710da18167077b7077e6dbd9dbc2d9b16e543a
Severin Rudie <srudie@mozilla.com>
2019-08-23 13:30:09 -0700

    Closes #2494: nits for FxA profile sign out

--- app/src/test/java/org/mozilla/tv/firefox/fxa/FxaRepoTest.kt
+++ app/src/test/java/org/mozilla/tv/firefox/fxa/FxaRepoTest.kt
@@ -127,7 +127,7 @@ class FxaRepoTest {
     fun `GIVEN profile has no avatar WHEN profile is converted to domain object THEN default resource image should be used`() {
         val profile = Profile("uid", null, null, null)
 
-        assertEquals(defaultProfileAvatarImage, profile.toDomainObject().avatar)
+        assertEquals(defaultProfileAvatarImage, profile.toDomainObject().avatarSetStrategy)
     }
 
     @Test
@@ -140,7 +140,7 @@ class FxaRepoTest {
         )
 
         profiles.map { it.toDomainObject() }
-            .map { it.avatar }
+            .map { it.avatarSetStrategy }
             .forEach { assertEquals(defaultProfileAvatarImage, it) }
     }
 
@@ -156,7 +156,7 @@ class FxaRepoTest {
         )
 
         profiles.map { it.toDomainObject() }
-            .map { it.avatar }
+            .map { it.avatarSetStrategy }
             .forEach { assertEquals(expectedStrategy, it) }
     }
 }
