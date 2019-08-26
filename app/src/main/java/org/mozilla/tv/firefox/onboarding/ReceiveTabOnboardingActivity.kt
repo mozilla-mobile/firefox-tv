8db0304ff043b5d395599e7083adf353928f63fa
Severin Rudie <srudie@mozilla.com>
2019-08-26 14:11:50 -0700

    Issue #2495: copy OnboardingActivity as ReceiveTabOnboardingActivity
    Other than the name, no changes were made to this file. It will be fixed in the next commit

---
+++ app/src/main/java/org/mozilla/tv/firefox/onboarding/ReceiveTabOnboardingActivity.kt
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.onboarding
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.content_onboarding.*
+import org.mozilla.tv.firefox.R
+import org.mozilla.tv.firefox.ext.serviceLocator
+
+class ReceiveTabOnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+
+        enable_turbo_mode.setOnClickListener { _ ->
+            setTurboMode(true)
+            finish()
+        }
+
+        disable_turbo_mode.setOnClickListener { _ ->
+            setTurboMode(false)
+            setResult(Activity.RESULT_OK, Intent())
+            finish()
+        }
+
+        setOnboardShown()
+    }
+
+    private fun setTurboMode(turboModeEnabled: Boolean) {
+        serviceLocator.turboMode.isEnabled = turboModeEnabled
+    }
+
+    private fun setOnboardShown() {
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .edit()
+                .putBoolean(ONBOARD_SHOWN_PREF, true)
+                .apply()
+    }
+
+    companion object {
+        const val ONBOARD_SHOWN_PREF = "onboard_shown"
+    }
+}
