bd94c631625a2d07fc3229e3be300b73d78d3db9
Severin Rudie <srudie@mozilla.com>
2019-09-11 14:10:46 -0700

    Issue #2794: implement ManualUpgradeStarter variants

--- app/src/main/java/org/mozilla/tv/firefox/manual/upgrade/ManualUpgradeStarter.kt
+++ app/src/main/java/org/mozilla/tv/firefox/manual/upgrade/ManualUpgradeStarter.kt
@@ -4,28 +4,47 @@
 
 package org.mozilla.tv.firefox.manual.upgrade
 
+import android.content.Context
+import android.preference.PreferenceManager
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
 /**
  * TODO
  */
 interface ManualUpgradeStarter {
-    fun maybeShow()
+    fun maybeShow(context: Context)
 }
 
-class DoNotShowUpgradeStarter() : ManualUpgradeStarter {
-    override fun maybeShow() { /* Noop */ }
+class DoNotShowUpgradeStarter : ManualUpgradeStarter {
+    override fun maybeShow(context: Context) { /* Noop */ }
 }
 
-class RequestUpgradeStarter() : ManualUpgradeStarter {
-    override fun maybeShow() {
-        // Check last time shown
-        // If time > X, start request upgrade activity
-        TODO()
+// TODO use different time library
+private const val UPGRADE_REQUEST_LAST_SHOWN_AT = "UPGRADE_REQUEST_LAST_SHOWN_AT"
+private val UTC = ZoneOffset.UTC
+private const val SHOW_REQUEST_EVERY_X_DAYS = 5L
+
+class RequestUpgradeStarter : ManualUpgradeStarter {
+
+    override fun maybeShow(context: Context) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val lastShownAtEpochSecond = preferences.getLong(UPGRADE_REQUEST_LAST_SHOWN_AT, 0)
+        val lastShownAt = LocalDateTime.ofEpochSecond(lastShownAtEpochSecond, 0, ZoneOffset.UTC)
+        val now = LocalDateTime.now(UTC)
+
+        if (lastShownAt.plusDays(SHOW_REQUEST_EVERY_X_DAYS) >= now) {
+            preferences.edit()
+                .putLong(UPGRADE_REQUEST_LAST_SHOWN_AT, now.toEpochSecond())
+                .apply()
+
+            context.startActivity(/* RequestUpgradeActivity */)
+        }
     }
 }
 
-class ForceUpgradeStarter() : ManualUpgradeStarter {
-    override fun maybeShow() {
-        // start force upgrade activity
-        TODO()
+class ForceUpgradeStarter : ManualUpgradeStarter {
+    override fun maybeShow(context: Context) {
+        context.startActivity(/* ForceUpgradeActivity */)
     }
 }
