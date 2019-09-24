1b942e176c71e908d5d761c512324d3115f644c2
Mitchell Hentges <mhentges@mozilla.com>
2019-09-24 14:19:49 -0700

    Moves additional data to yaml, removes install-sdk.sh

--- taskcluster/firefox_tv_taskgraph/email.py
+++ taskcluster/firefox_tv_taskgraph/email.py
@@ -13,24 +13,23 @@ NOTIFY_EMAIL_ADDRESS = 'firefox-tv@mozilla.com'
 
 @transforms.add
 def email_task(config, tasks):
-    tag = os.environ['GIT_TAG']
-    content = 'Automation for this release is ready. Please: \n' \
-              '* Download the APK and attach it to the [Github release](https://github.com/mozilla-mobile/firefox-tv/releases/tag/{})\n'.format(tag) + \
-              '* [Deploy the new release on Amazon](https://developer.amazon.com/apps-and-games/console/app/amzn1.devportal.mobileapp.7f334089688646ef8953d041021029c9/release/amzn1.devportal.apprelease.4ca3990c43f34101bf5729543343747a/general/detail)'
+    tag = os.environ.get("GIT_TAG", "UNDEFINED_TAG")
 
     for task in tasks:
-        to_address = task["worker"]["to-address"]
-        del task["worker"]["to-address"]
+        to_address = task["worker"].pop("to-address")
+        content = task["worker"].pop("content")
+        subject = task["worker"].pop("subject")
+        link_text = task["worker"].pop("link-text")
         task["scopes"] = ["queue:route:notify.email.{}.on-completed".format(to_address)]
         task["routes"] = ["notify.email.{}.on-completed".format(to_address)]
         task["extra"] = {
             "notify": {
                 "email": {
-                    "content": content,
-                    "subject": "Release {} is ready for deployment".format(tag),
+                    "content": content.format(tag=tag),
+                    "subject": subject.format(tag=tag),
                     "link": {
                         "href": {"artifact-reference": "<sign/public/build/target.apk>"},
-                        "text": "{} APK".format(tag)
+                        "text": link_text.format(tag=tag)
                     },
                 }
             }
