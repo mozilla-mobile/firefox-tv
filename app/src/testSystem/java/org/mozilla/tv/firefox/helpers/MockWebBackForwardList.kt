428a40599ccb52d6dbdf118c137d5868b0393065
Michael Comella <michael.l.comella@gmail.com>
2019-09-25 14:18:00 -0700

    Issue #2805: Add MockWebBackForwardList.

---
+++ app/src/testSystem/java/org/mozilla/tv/firefox/helpers/MockWebBackForwardList.kt
@@ -0,0 +1,58 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.tv.firefox.helpers
+
+import android.graphics.Bitmap
+import android.webkit.WebBackForwardList
+import android.webkit.WebHistoryItem
+
+/**
+ * An implementation of the [WebBackForwardList] abstract class for testing.
+ */
+class MockWebBackForwardList(
+    private val backingList: List<WebHistoryItem>,
+    private val mockCurrentIndex: Int = 0
+) : WebBackForwardList() {
+
+    override fun getSize(): Int = backingList.size
+    override fun getItemAtIndex(index: Int): WebHistoryItem = backingList[index]
+
+    override fun getCurrentIndex(): Int = mockCurrentIndex
+    override fun getCurrentItem(): WebHistoryItem? = backingList[mockCurrentIndex]
+
+    override fun clone(): WebBackForwardList {
+        TODO("not implemented: not needed yet")
+    }
+}
+
+/**
+ * An implementation of the [WebHistoryItem] abstract class for testing.
+ */
+class MockWebHistoryItem(
+    private val mockOriginalUrl: String? = null
+) : WebHistoryItem() {
+
+    override fun getOriginalUrl(): String? = mockOriginalUrl
+
+    override fun getUrl(): String? {
+        TODO("not implemented: not needed yet")
+    }
+
+    override fun getFavicon(): Bitmap? {
+        TODO("not implemented: not needed yet")
+    }
+
+    override fun getTitle(): String? {
+        TODO("not implemented: not needed yet")
+    }
+
+    override fun clone(): WebHistoryItem {
+        TODO("not implemented: not needed yet")
+    }
+}
+
+fun List<WebHistoryItem>.toMockWebBackForwardList(): MockWebBackForwardList {
+    return MockWebBackForwardList(this)
+}
