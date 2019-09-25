d10f35ec4477fd71a3c6e1c13eaffd7838270220
Mitchell Hentges <mhentges@mozilla.com>
2019-09-25 15:35:38 -0700

    Moves dockerfile into taskgraph

--- taskcluster/firefox_tv_taskgraph/__init__.py
+++ taskcluster/firefox_tv_taskgraph/__init__.py
@@ -12,7 +12,7 @@ def register(graph_config):
     Import all modules that are siblings of this one, triggering decorators in
     the process.
     """
-    _import_modules(["job", "worker_types", "target_tasks"])
+    _import_modules(["worker_types", "target_tasks"])
 
 
 def _import_modules(modules):
