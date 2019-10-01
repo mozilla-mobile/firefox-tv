e914a74d622fb956c777adbe3735a6f142ece5f5
Mitchell Hentges <mhentges@mozilla.com>
2019-09-30 20:35:49 -0700

    Don't push LAT releases to amazon

--- taskcluster/firefox_tv_taskgraph/email.py
+++ taskcluster/firefox_tv_taskgraph/email.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 import os
 
 from taskgraph.transforms.base import TransformSequence
-
+from taskgraph.util.schema import resolve_keyed_by
 
 transforms = TransformSequence()
 
@@ -18,23 +18,26 @@ NOTIFY_EMAIL_ADDRESS = 'firefox-tv@mozilla.com'
 @transforms.add
 def email_task(config, tasks):
     tag = config.params.get("head_tag")
+    release_type = config.params.get("release_type")
 
     for task in tasks:
+        resolve_keyed_by(task, "worker.content", task["name"], **{
+            "release-type": release_type
+        })
+
+        if release_type == 'lat':
+            task["dependencies"].pop("push")
+
         to_address = task["worker"].pop("to-address")
         content = task["worker"].pop("content")
         subject = task["worker"].pop("subject")
-        link_text = task["worker"].pop("link-text")
         task["scopes"] = ["queue:route:notify.email.{}.on-completed".format(to_address)]
         task["routes"] = ["notify.email.{}.on-completed".format(to_address)]
         task["extra"] = {
             "notify": {
                 "email": {
-                    "content": content.format(tag=tag),
+                    "content": {"artifact-reference": content.format(tag=tag)},
                     "subject": subject.format(tag=tag),
-                    "link": {
-                        "href": {"artifact-reference": "<sign/public/build/target.apk>"},
-                        "text": link_text.format(tag=tag)
-                    },
                 }
             }
         }
--- taskcluster/firefox_tv_taskgraph/target_tasks.py
+++ taskcluster/firefox_tv_taskgraph/target_tasks.py
@@ -7,11 +7,22 @@ from __future__ import absolute_import, print_function, unicode_literals
 from taskgraph.target_tasks import _target_task, standard_filter
 
 
-@_target_task('default')
-def target_tasks_default(full_task_graph, parameters, graph_config):
-    """Target the tasks which have indicated they should be run on this project
-    via the `run_on_projects` attributes."""
-    def filter(task, params):
-        return standard_filter(task, params)
+def tag_filter(target_release_type, task, params):
+    release_type = task.attributes.get("release-type")
+    return standard_filter(task, params) and (release_type is None or release_type == target_release_type)
 
+
+@_target_task("default")
+def target_tasks_default(full_task_graph, parameters, graph_config):
+    filter = standard_filter
     return [l for l, task in full_task_graph.tasks.iteritems() if filter(task, parameters)]
+
+
+@_target_task("production")
+def target_tasks_production(full_task_graph, parameters, graph_config):
+    return [l for l, task in full_task_graph.tasks.iteritems() if tag_filter("production", task, parameters)]
+
+
+@_target_task("lat")
+def target_tasks_production(full_task_graph, parameters, graph_config):
+    return [l for l, task in full_task_graph.tasks.iteritems() if tag_filter("lat", task, parameters)]
