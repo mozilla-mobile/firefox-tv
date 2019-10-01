9f384eb086af69dae38a89e547360bb23d751e81
Mitchell Hentges <mhentges@mozilla.com>
2019-09-30 20:35:49 -0700

    Don't push LAT releases to amazon

--- taskcluster/firefox_tv_taskgraph/__init__.py
+++ taskcluster/firefox_tv_taskgraph/__init__.py
@@ -20,6 +20,7 @@ def register(graph_config):
     _import_modules(["worker_types", "target_tasks"])
     extend_parameters_schema({
         Required("head_tag"): Any(text_type, None),
+        Required("release_type"): Any(text_type, None),
     })
 
 
@@ -30,9 +31,17 @@ def _import_modules(modules):
 
 def get_decision_parameters(graph_config, parameters):
     parameters["head_tag"] = None
+    parameters["release_type"] = None
     if parameters["tasks_for"] == "github-release":
         head_tag = os.environ.get("MOBILE_HEAD_TAG")
         if head_tag is None:
             raise ValueError("Cannot run github-release if the environment variable "
                              "'MOBILE_HEAD_TAG' is not defined")
-        parameters["head_tag"] = head_tag.decode("utf-8")
+        head_tag = head_tag.decode("utf-8")
+        release_type = "lat" if "LAT" in head_tag else "production"
+        parameters["head_tag"] = head_tag
+        parameters["release_type"] = release_type
+        if release_type == "lat":
+            parameters["target_tasks_method"] = "lat"
+        else:
+            parameters["target_tasks_method"] = "production"
